2048f15f2d09adc966b5301eb770dd1f7573f7c5
DevSchmidtchen <contact@schmidtchen.me>
2021-11-23 15:46:08 +0100

    worked on login screen

--- app/lib/profile/pages/profile.dart
+++ app/lib/profile/pages/profile.dart
@@ -1,12 +1,77 @@
+import 'package:app/app/widgets/headings.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Heading('Login to Lab Server'),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: usernameController,
+            decoration: InputDecoration(
+              labelText: 'Username',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Username: ${usernameController.text}'),
+                          Text('Password: ${passwordController.text}'),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('Login'))
+        ],
+      ),
     );
   }
 }
