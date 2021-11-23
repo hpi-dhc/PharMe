d2add8b7833772f1f6259d4ebe154572b6640a76
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-23 15:51:22 +0100

    Add ENV and ConfigService
    Co-authored-by: kolioOtSofia <n.genchev@hotmail.com>
    Co-authored-by: toalaah <samuel.kunst@gmail.com>

--- annotation-server/src/app.module.ts
+++ annotation-server/src/app.module.ts
@@ -1,19 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { __env } from './constants';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'annotation_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: __env ? `.env.${__env}` : '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('ANNOTATION_DB_HOST'),
+        port: configService.get<number>('ANNOTATION_DB_PORT'),
+        username: configService.get<string>('ANNOTATION_DB_USER'),
+        password: configService.get<string>('ANNOTATION_DB_PASS'),
+        database: configService.get<string>('ANNOTATION_DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
