ecbd1168c6c632c7999d82303509627265834bd8
Benjamin Frost <bfrost.schule@gmail.com>
2021-11-23 12:08:44 +0100

    Add PG and TypeORM
    Co-authored-by: kolioOtSofia <n.genchev@hotmail.com>

--- annotation-server/src/app.module.ts
+++ annotation-server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'annotation_server',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
