357a56eb04ba4b2642b73b5d32a43afcbd17875f
DevSchmidtchen <contact@schmidtchen.me>
2021-12-07 16:09:34 +0100

    adds http post request as well as validation

--- app/lib/profile/pages/cubit.dart
+++ app/lib/profile/pages/cubit.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:http/http.dart' as http;
 
 part 'cubit.freezed.dart';
 
@@ -7,17 +11,35 @@ class ProfileCubit extends Cubit<ProfileState> {
   ProfileCubit() : super(ProfileState.initial());
 
   Future<void> login(String username, String password) async {
-    emit(ProfileState.loading());
+    emit(ProfileState.loading('Loading genomic data...'));
 
     try {
+      // TODO(DevSchmidtchen): Replace with lab server API endpoint address
+      final response = await http.post(
+        Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'username': username,
+          'password': password,
+        }),
+      );
+
+      /* 
       await Future.delayed(
         const Duration(seconds: 10),
         () => {},
       );
+      */
 
-      emit(ProfileState.loaded());
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        emit(ProfileState.loaded(response.body));
+      } else {
+        emit(ProfileState.error('Error'));
+      }
     } catch (error) {
-      emit(ProfileState.error());
+      emit(ProfileState.error('Error'));
     }
   }
 }
@@ -25,7 +47,7 @@ class ProfileCubit extends Cubit<ProfileState> {
 @freezed
 class ProfileState with _$ProfileState {
   const factory ProfileState.initial() = _InitialState;
-  const factory ProfileState.loading() = _LoadingState;
-  const factory ProfileState.loaded() = _LoadedState;
-  const factory ProfileState.error() = _ErrorState;
+  const factory ProfileState.loading(String message) = LoadingState;
+  const factory ProfileState.loaded(String message) = LoadedState;
+  const factory ProfileState.error(String error) = _ErrorState;
 }
--- app/lib/profile/pages/profile.dart
+++ app/lib/profile/pages/profile.dart
@@ -14,6 +14,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
@@ -25,62 +26,98 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return BlocProvider<ProfileCubit>(
       create: (_) => ProfileCubit(),
-      child: BlocBuilder<ProfileCubit, ProfileState>(
+      child: BlocConsumer<ProfileCubit, ProfileState>(
+        listener: (context, state) {
+          state.maybeWhen(
+            error: (error) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Error logging in! $error'),
+                ),
+              );
+            },
+            orElse: () {},
+          );
+        },
         builder: (context, state) {
           return Padding(
             padding: const EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Heading('Login to Lab Server'),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Heading('Login to Lab Server'),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    context.read<ProfileCubit>().login(
-                        usernameController.text, passwordController.text);
-                  },
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: Size.fromHeight(50)),
-                  child: state.when(
-                    initial: () => Text('Login'),
-                    loading: () => SizedBox(
-                      width: 25,
-                      height: 25,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 3,
-                        value: null,
-                        color: Colors.white,
-                      ),
+                  TextFormField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      border: OutlineInputBorder(),
                     ),
-                    loaded: () => Text('Login'),
-                    error: () => Text('Login'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your username!';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password!';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        context.read<ProfileCubit>().login(
+                            usernameController.text, passwordController.text);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: Size.fromHeight(50)),
+                    child: state.maybeWhen(
+                      loading: (message) => SizedBox(
+                        width: 25,
+                        height: 25,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 3,
+                          value: null,
+                          color: Colors.white,
+                        ),
+                      ),
+                      orElse: () => Text('Login'),
+                    ),
+                  ),
+                  if (state is LoadingState || state is LoadedState)
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      child: state.maybeWhen(
+                        loading: (message) => Text(message),
+                        loaded: (message) => Text(message),
+                        orElse: () => null,
+                      ),
+                    )
+                ],
+              ),
             ),
           );
         },
