166d74f4444cccb528cc61e06ad269f02dd53533
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-12-07 14:36:56 +0100

    Update Contribution Guide to have correct setup

--- annotation-server/src/clinical_annotation.entity.ts
+++ annotation-server/src/clinical_annotation.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, PrimaryColumn } from 'typeorm';
+import { Entity, Column, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class ClinicalAnnotation {
--- annotation-server/src/clinical_annotation.module.ts
+++ annotation-server/src/clinical_annotation.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClinicalAnnotationService } from './clinical_annotation.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClinicalAnnotation } from './clinical_annotation.entity';
 import { ClinicalAnnotationsController } from './clinical_annotation.controller';
 import { HttpModule } from '@nestjs/axios';
