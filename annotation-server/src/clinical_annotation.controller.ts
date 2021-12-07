f34a4a9b4aa2d60ded7cf8556484f2f5179d3530
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-12-07 14:36:56 +0100

    Update Contribution Guide to have correct setup

--- annotation-server/src/clinical_annotation.controller.ts
+++ annotation-server/src/clinical_annotation.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ClinicalAnnotationService } from './clinical_annotation.service';
-import { ClinicalAnnotation } from './clinical_annotation.entity';
 
 @Controller('clinical_annotations')
 export class ClinicalAnnotationsController {
