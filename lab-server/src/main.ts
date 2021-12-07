9614ec3d42fe036e41da28dc4d0f5fcfb9241bc3
Nikola Genchev <n.genchev@hotmail.com>
2021-12-07 17:55:51 +0100

    adding mock version of the log in

--- lab-server/src/main.ts
+++ lab-server/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Lab Server')
