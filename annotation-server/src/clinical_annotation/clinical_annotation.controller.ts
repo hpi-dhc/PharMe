5e375dfc689fe6fcb1ccb3c257585238f3888666
DevSchmidtchen <contact@schmidtchen.me>
2022-01-03 12:54:02 +0100

    fixes saving to database and refactors code

--- annotation-server/src/clinical_annotation/clinical_annotation.controller.ts
+++ annotation-server/src/clinical_annotation/clinical_annotation.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { ClinicalAnnotation } from './clinical_annotation.entity';
 import { ClinicalAnnotationService } from './clinical_annotation.service';
 
 @Controller('clinical_annotations')
@@ -6,7 +7,7 @@ export class ClinicalAnnotationsController {
   constructor(private clinicalAnnotationsService: ClinicalAnnotationService) {}
 
   @Get()
-  async findAll(): Promise<ListableAnnotation[]> {
+  async findAll(): Promise<ClinicalAnnotation[]> {
     return this.clinicalAnnotationsService.findAll();
   }
 
