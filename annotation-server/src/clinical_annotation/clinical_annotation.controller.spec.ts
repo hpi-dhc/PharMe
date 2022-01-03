024ca43b7ad97c4a219dcfad8ec839c775cf0744
DevSchmidtchen <contact@schmidtchen.me>
2022-01-03 16:25:30 +0100

    fix tests

--- annotation-server/src/clinical_annotation/clinical_annotation.controller.spec.ts
+++ annotation-server/src/clinical_annotation/clinical_annotation.controller.spec.ts
@@ -6,30 +6,11 @@ import { ClinicalAnnotationService } from './clinical_annotation.service';
 
 describe('ClinicalAnnotationsController (e2e)', () => {
   let app: INestApplication;
-  let clinicalAnnotationsService = {
-    findAll: () => {
-      {
-        clinicalAnnotationId: '981204774';
-        variants: 'rs1799971';
-        genes: 'OPRM1';
-        levelOfEvidence: '4';
-        score: '-2.0';
-        phenotypeCategory: 'Efficacy';
-        drugs: 'Drugs used in nicotine dependence;nicotine';
-        phenotypes: 'Tobacco Use Disorder';
-        pharmkgbUrl: 'https://www.pharmgkb.org/clinicalAnnotation/981204774';
-      }
-    },
-    synchronize: () => {},
-  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider(ClinicalAnnotationService)
-      .useValue(clinicalAnnotationsService)
-      .compile();
+    }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
