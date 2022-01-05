e90215243f546c785dbf0c5da0a1218fe4f6c11a
Jannis Baum <jannis.baum@gmail.com>
2022-01-05 15:42:00 +0100

    refactor(interview-analysis): restructure

--- interview-analysis/analysis.py
+++ interview-analysis/analysis.py
@@ -6,7 +6,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class NLProcessor:
     __filter_chars = ['.', ',', '?', ';', '"', '#', '\'', '!', '‘', '’', '“', '”', '…', ':', '_', '*'] 
-    __stop_words_path = 'stop_words_en.txt'
+    __stop_words_path = 'nlp-resources/stop_words_en.txt'
     __stop_words = None
     __word_vectors_id = 'word2vec-google-news-300'
     __word_vectors = None
