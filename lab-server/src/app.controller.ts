4480ae19a00d8d8ee6e71aefffd6c6d297ce9a41
Samuel Kunst <samuel.kunst@gmail.com>
2022-02-07 14:18:37 +0100

    feat: replace root GET with health check

--- lab-server/src/app.controller.ts
+++ lab-server/src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor() {}
 
-  @Get()
+  @Get('/health')
   getHello(): string {
-    return this.appService.getHello();
+    return 'ok';
   }
 }
