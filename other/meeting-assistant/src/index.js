e073c7ae5b3a0cd378907b5f60c019af03a22ee5
Benjamin Frost <bfrost.schule@gmail.com>
2022-02-14 16:45:31 +0100

    feat: add main functionality

--- other/meeting-assistant/src/index.js
+++ other/meeting-assistant/src/index.js
@@ -1,16 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const moment = require('moment');
 
-try {
-  const users = core.getInput('users');
-  if (!users) {
-    throw new Error('Could not find a list of users');
+async function run() {
+  try {
+    // Get input
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const USERS_INPUT = core.getInput('users', { required: true });
+    const ISSUE_NUMBER = core.getInput('issue-number', { required: true });
+
+    // Select user
+    const users = JSON.parse(USERS_INPUT);
+    const selectedUser = users[moment().week() % users.length];
+
+    // Assign user to issue
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    await octokit.rest.issues.addAssignees({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      issue_number: ISSUE_NUMBER,
+      assignees: [selectedUser.github],
+    });
+
+    // Set output
+    core.setOutput('telegram', selectedUser.telegram);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  console.log(users);
-
-  core.setOutput('telegram', users[0].telegram);
-  console.log(`Hello ${users[0].telegram}!`);
-} catch (error) {
-  core.setFailed(error.message);
 }
+run();
