9b51b37edf7923267b220386cdd113718f03079a
Benjamin Frost <bfrost.schule@gmail.com>
2022-03-09 21:22:01 +0100

    feat: add jest-badges-generator workflow

---
+++ other/jest-badges-generator/src/helpers.js
@@ -0,0 +1,55 @@
+import { pathExists, readJson } from 'fs-extra';
+
+const isUndefined = (element) => element?.pct === undefined;
+
+export const isJestCoverageReportAvailable = async (jestSummaryPath) => {
+  const coverageExists = await pathExists(jestSummaryPath);
+  if (!coverageExists) return false;
+
+  const data = await readJson(jestSummaryPath);
+  if (!data || !data.total) return false;
+
+  if (
+    isUndefined(data.total.branches) ||
+    isUndefined(data.total.functions) ||
+    isUndefined(data.total.lines) ||
+    isUndefined(data.total.statements)
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+export const doBadgesExist = async (badgeOutputDir) => {
+  const files = [
+    'coverage-branches.svg',
+    'coverage-functions.svg',
+    'coverage-jest coverage.svg',
+    'coverage-lines.svg',
+    'coverage-statements.svg',
+  ];
+
+  const exist = await Promise.all(
+    files.map((file) => pathExists(`${badgeOutputDir}/${file}`))
+  );
+
+  return exist.every((el) => el === true);
+};
+
+export const hasCoverageEvolved = async (badgesExist, badgeOutputDir) => {
+  if (!badgesExist) return true;
+
+  const code = await exec('git diff', ['--quiet', `${badgeOutputDir}/*`], {
+    ignoreReturnCode: true,
+  });
+
+  const hasChanged = code === 1;
+  return hasChanged;
+};
+
+export const pushBadges = async (badgeOutputDir) => {
+  await exec('git add', [badgeOutputDir]);
+  await exec('git commit', ['-m', 'docs: updating coverage badges']);
+  await exec('git push');
+};
