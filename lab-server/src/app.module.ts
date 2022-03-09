354d48912e087e0af76a418ba52c3a5fcf2905d5
Benjamin Frost <bfrost.schule@gmail.com>
2022-03-09 18:43:48 +0100

    refactor(lab-server): move app configs to folder

--- lab-server/src/app.module.ts
+++ lab-server/src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {
-  AuthGuard,
-  KeycloakConnectModule,
-  TokenValidation,
-} from 'nest-keycloak-connect';
+import { ConfigModule } from '@nestjs/config';
+import { KeycloakModule, KeycloakProviders } from './configs/keycloak.config';
+import { OrmModule } from './configs/orm.config';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -14,37 +9,11 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    KeycloakConnectModule.registerAsync({
-      useFactory: (configService: ConfigService) => ({
-        authServerUrl: configService.get<string>('KEYCLOAK_AUTH_SERVER_URL'),
-        realm: configService.get<string>('KEYCLOAK_REALM'),
-        clientId: configService.get<string>('KEYCLOAK_CLIENT_ID'),
-        secret: configService.get<string>('KEYCLOAK_SECRET'),
-        tokenValidation: TokenValidation.OFFLINE,
-      }),
-      inject: [ConfigService],
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('LAB_SERVER_DB_HOST'),
-        port: configService.get<number>('LAB_SERVER_DB_PORT'),
-        username: configService.get<string>('LAB_SERVER_DB_USER'),
-        password: configService.get<string>('LAB_SERVER_DB_PASS'),
-        database: configService.get<string>('LAB_SERVER_DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    KeycloakModule,
+    OrmModule,
     UsersModule,
   ],
   controllers: [],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
+  providers: [...KeycloakProviders],
 })
 export class AppModule {}
