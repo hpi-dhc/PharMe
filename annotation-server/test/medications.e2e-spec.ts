88b10ba53adecd00eb5785116333dbb9211824ea
Jannis Baum <jannis.baum@gmail.com>
2022-04-05 16:39:38 +0200

    test(annotation-server): add end-to-end test for clinical annotation
    Co-authored-by: Matti Schmidt <contact@schmidtchen.me>

--- annotation-server/test/medications.e2e-spec.ts
+++ annotation-server/test/medications.e2e-spec.ts
@@ -1,13 +1,19 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as cookieParser from 'cookie-parser';
 import * as request from 'supertest';
+import { Repository } from 'typeorm';
 
-import { MedicationsService } from '../src/medications/medications.service';
 import { AppModule } from './../src/app.module';
+import { GeneVariant } from './../src/medications/interfaces/geneVariant.interface';
+import { Medication } from './../src/medications/medication.entity';
+import { MedicationsService } from './../src/medications/medications.service';
 
 describe('MedicationsController (e2e)', () => {
     let app: INestApplication;
     let medicationService: MedicationsService;
+    let medicationRepository: Repository<Medication>;
 
     beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,7 +21,12 @@ describe('MedicationsController (e2e)', () => {
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.use(cookieParser());
         await app.init();
+
+        medicationRepository = moduleFixture.get<Repository<Medication>>(
+            getRepositoryToken(Medication),
+        );
 
         medicationService =
             moduleFixture.get<MedicationsService>(MedicationsService);
@@ -33,6 +44,42 @@ describe('MedicationsController (e2e)', () => {
         getResponse.expect(200);
         expect((await getResponse).body.length).toBe(100);
     }, 20000);
+
+    it('should return some annotations for Ibuprofen', (done) => {
+        const mockMedication = new Medication();
+        mockMedication.name = 'mock-ibuprofen';
+        mockMedication.pharmgkbId = 'PA449957';
+        mockMedication.synonyms = [];
+        medicationRepository.save(mockMedication).then(({ id }) => {
+            const geneVariants: Array<GeneVariant> = [
+                {
+                    gene: 'CYP2C9',
+                    variant1: '*1',
+                    variant2: '*3',
+                },
+            ];
+            request(app.getHttpServer())
+                .get(`/medications/${id}/annotations`)
+                .set('Cookie', [`variants=${JSON.stringify(geneVariants)}`])
+                .send()
+                .expect(
+                    200,
+                    [
+                        {
+                            gene: 'CYP2C9',
+                            description:
+                                'The CYP2C9*1 allele is assigned as a normal function allele by CPIC. Patients carrying CYP2C9*1 allele in combination with another normal function allele may have increased metabolism of ibuprofen as compared to patients carrying at least one copy of a decreased or no function allele. However, conflicting evidence has been reported. Other genetic and clinical factors may also influence metabolism of ibuprofen. This annotation only covers the pharmacokinetic relationship between CYP2C9 and ibuprofen and does not include evidence about clinical outcomes.',
+                        },
+                        {
+                            gene: 'CYP2C9',
+                            description:
+                                'The CYP2C9*3 allele is assigned as a no function allele by CPIC. Patients carrying the CYP2C9*3 allele in combination with a normal, decreased, or no function allele may have decreased metabolism of ibuprofen as compared to patients with two normal function alleles. Other genetic and clinical factors may also influence metabolism of ibuprofen. This annotation only covers the pharmacokinetic relationship between CYP2C9 and ibuprofen and does not include evidence about clinical outcomes.',
+                        },
+                    ],
+                    done,
+                );
+        });
+    });
 
     afterAll(async () => {
         await medicationService.clearAllMedicationData();
