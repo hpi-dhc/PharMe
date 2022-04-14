f1750cbec94c2ab191e36ba14c97b85883ee82b9
Benjamin Frost <bfrost.schule@gmail.com>
2022-04-15 01:18:04 +0200

    feat(app): add pgx header card

--- app/lib/pgx/pages/pgx.dart
+++ app/lib/pgx/pages/pgx.dart
@@ -1,12 +1,64 @@
+import 'package:black_hole_flutter/black_hole_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../common/theme.dart';
 
 class PgxPage extends StatelessWidget {
   const PgxPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Pgx'),
+    return Column(
+      children: [
+        _buildHeaderCard(context),
+      ],
+    );
+  }
+
+  Widget _buildHeaderCard(BuildContext context) {
+    return Container(
+      height: 150,
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            PharmeTheme.primaryColor.shade500,
+            PharmeTheme.primaryColor.shade800,
+          ],
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: SvgPicture.asset(
+              'assets/images/pgx_faq.svg',
+              width: 120,
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pharmacogenomics',
+                  style: context.textTheme.headline6!
+                      .copyWith(color: Colors.white),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'On this page we will answer common questions regarding PGx',
+                  style: context.textTheme.bodyText2!
+                      .copyWith(color: Colors.white),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
