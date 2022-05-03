32f92dabcd99f0721f72d6b4fc507784b2ce7d64
Nikola Genchev <n.genchev@hotmail.com>
2022-05-03 15:55:42 +0200

    chore(app): found alternative for sliding up panel

--- app/lib/medications/pages/overview/page.dart
+++ app/lib/medications/pages/overview/page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_sliding_up_panel/sliding_up_panel_widget.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../common/constants.dart';
@@ -11,6 +13,8 @@ import '../../../common/module.dart';
 import '../../../common/theme.dart';
 import '../../models/medication.dart';
 import 'cubit.dart';
+
+final panelController = SlidingUpPanelController();
 
 class MedicationsOverviewPage extends HookWidget {
   const MedicationsOverviewPage({Key? key}) : super(key: key);
@@ -26,69 +30,118 @@ class MedicationsOverviewPage extends HookWidget {
       create: (context) => MedicationsOverviewCubit(),
       child: BlocBuilder<MedicationsOverviewCubit, MedicationsOverviewState>(
         builder: (context, state) {
-          return RoundedCard(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: CupertinoSearchTextField(
-                    controller: searchController,
-                    onChanged: (value) async {
-                      if (value == '') {
-                        context.read<MedicationsOverviewCubit>().setState(
-                              MedicationsOverviewState.initial(),
-                            );
-                        return;
-                      }
-                      if (searchTimeout != null) {
-                        searchTimeout!.cancel();
-                      }
-                      searchTimeout = Timer(
-                        duration,
-                        () async {
-                          final requestUri = annotationServerUrl.replace(
-                            path: 'api/v1/medications',
-                            queryParameters: {'search': value},
-                          );
-                          context.read<MedicationsOverviewCubit>().setState(
-                                MedicationsOverviewState.loading(),
-                              );
-                          final response = await http.get(requestUri);
-                          if (response.statusCode != 200) {
-                            context.read<MedicationsOverviewCubit>().setState(
-                                  MedicationsOverviewState.error(),
-                                );
-                            return;
-                          }
-                          foundMedications.value =
-                              medicationsFromHTTPResponse(response);
-                          context.read<MedicationsOverviewCubit>().setState(
-                                MedicationsOverviewState.loaded(
-                                  foundMedications.value,
-                                ),
-                              );
-                          print(foundMedications.value.length);
-                        },
-                      );
-                    },
+          return Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      PharmeTheme.primaryColor,
+                      PharmeTheme.secondaryColor,
+                    ],
                   ),
                 ),
-                state.when(
-                  initial: Container.new,
-                  error: () => Text('smth went wrong'),
-                  loaded: (medications) => Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(children: [
-                        for (final el in medications) ...[
-                          MedicationCard(onTap: () {}, medicationName: el.name),
-                        ]
-                      ]),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset('assets/images/logo.svg'),
+                    Text(
+                      'Type in a medication',
+                      style: PharmeTheme.textTheme.bodyLarge!
+                          .copyWith(color: Colors.white),
                     ),
-                  ),
-                  loading: () => Center(child: CircularProgressIndicator()),
+                    Icon(
+                      Icons.arrow_downward,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 120),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SlidingUpPanelWidget(
+                controlHeight: 150,
+                panelController: panelController,
+                child: RoundedCard(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: CupertinoSearchTextField(
+                          controller: searchController,
+                          onChanged: (value) async {
+                            if (value == '') {
+                              context.read<MedicationsOverviewCubit>().setState(
+                                    MedicationsOverviewState.initial(),
+                                  );
+                              return;
+                            }
+                            if (searchTimeout != null) {
+                              searchTimeout!.cancel();
+                            }
+                            searchTimeout = Timer(
+                              duration,
+                              () async {
+                                final requestUri = annotationServerUrl.replace(
+                                  path: 'api/v1/medications',
+                                  queryParameters: {'search': value},
+                                );
+                                context
+                                    .read<MedicationsOverviewCubit>()
+                                    .setState(
+                                      MedicationsOverviewState.loading(),
+                                    );
+                                final response = await http.get(requestUri);
+                                if (response.statusCode != 200) {
+                                  context
+                                      .read<MedicationsOverviewCubit>()
+                                      .setState(
+                                        MedicationsOverviewState.error(),
+                                      );
+                                  return;
+                                }
+                                foundMedications.value =
+                                    medicationsFromHTTPResponse(response);
+                                context
+                                    .read<MedicationsOverviewCubit>()
+                                    .setState(
+                                      MedicationsOverviewState.loaded(
+                                        foundMedications.value,
+                                      ),
+                                    );
+                                print(foundMedications.value.length);
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      state.when(
+                        initial: Container.new,
+                        error: () => Text('smth went wrong'),
+                        loaded: (medications) => Flexible(
+                          child: ListView.builder(
+                            itemCount: medications.length,
+                            itemBuilder: (context, index) {
+                              final el = medications[index];
+                              return MedicationCard(
+                                onTap: () {},
+                                medicationName: el.name,
+                              );
+                            },
+                          ),
+                        ),
+                        loading: () =>
+                            Center(child: CircularProgressIndicator()),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
