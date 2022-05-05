44e0619e92689185e04182b433f6b7edc2edebe5
Jannis Baum <jannis.baum@gmail.com>
2022-05-05 15:17:59 +0200

    feat(annotation-server): monthly data updates
    Co-authored-by: Matti Schmidt <contact@schmidtchen.me>

--- annotation-server/src/app.service.ts
+++ annotation-server/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, SchedulerRegistry } from '@nestjs/schedule';
 
 import { GenePhenotypesService } from './gene-phenotypes/gene-phenotypes.service';
 import { GuidelinesService } from './guidelines/guidelines.service';
@@ -7,11 +8,13 @@ import { MedicationsService } from './medications/medications.service';
 @Injectable()
 export class AppService {
     constructor(
+        private schedulerRegistry: SchedulerRegistry,
         private medicationService: MedicationsService,
         private guidelineService: GuidelinesService,
         private genephenotypeService: GenePhenotypesService,
     ) {}
 
+    @Cron('0 0 1 * *', { name: 'monthlyUpdates' })
     async initializeDatabase(): Promise<void> {
         await this.medicationService.fetchAllMedications();
         await this.genephenotypeService.fetchGenePhenotypes();
