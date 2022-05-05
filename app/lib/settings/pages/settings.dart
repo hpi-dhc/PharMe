23c49126d08fe80d76c6c578f7d90874f5827107
Benjamin Frost <bfrost.schule@gmail.com>
2022-05-05 15:28:54 +0200

    feat(app): localize settings page

--- app/lib/settings/pages/settings.dart
+++ app/lib/settings/pages/settings.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../common/module.dart';
 import '../../common/routing/router.dart';
 import '../utils.dart';
 
@@ -9,39 +10,36 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.delete),
-            title: Text('Delete App Data'),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () => showDialog(
-              context: context,
-              builder: (_) => _deleteDataDialog(context),
-            ),
+    return ListView(children: [
+      Card(
+        child: ListTile(
+          leading: Icon(Icons.delete),
+          title: Text(context.l10n.settings_page_delete_data),
+          trailing: Icon(Icons.chevron_right),
+          onTap: () => showDialog(
+            context: context,
+            builder: (_) => _deleteDataDialog(context),
           ),
         ),
-      ],
-    );
+      ),
+    ]);
   }
 
   Widget _deleteDataDialog(BuildContext context) {
     return AlertDialog(
-      title: Text('Delete App Data'),
-      content:
-          Text('Are you sure that you really want to delete all app data?'),
+      title: Text(context.l10n.settings_page_delete_data),
+      content: Text(context.l10n.settings_page_delete_data_text),
       actions: [
         TextButton(
           onPressed: context.router.root.pop,
-          child: Text('Cancel'),
+          child: Text(context.l10n.settings_page_cancel),
         ),
         TextButton(
           onPressed: () async {
             await deleteAllAppData();
             await context.router.replaceAll([LoginRouter()]);
           },
-          child: Text('Continue'),
+          child: Text(context.l10n.settings_page_continue),
         ),
       ],
     );
