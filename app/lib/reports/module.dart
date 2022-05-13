6e3de066c0c01bc19dd2bc1b60afe7ec86db1b2b
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2022-05-13 12:00:42 +0200

    refactor(app, med:details): move medication details out of common

--- app/lib/reports/module.dart
+++ app/lib/reports/module.dart
@@ -1,10 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 
-import '../common/pages/medication_details.dart';
+import '../details/pages/details.dart';
 import 'pages/reports.dart';
 
 // We need to expose all pages for AutoRouter
-export '../common/pages/medication_details.dart';
+export '../details/module.dart';
 export 'pages/reports.dart';
 
 const reportsRoutes = AutoRoute(
