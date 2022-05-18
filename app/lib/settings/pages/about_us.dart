2702730b0500e6d3b28a2773d67059e3328f49df
DevSchmidtchen <contact@schmidtchen.me>
2022-05-18 11:55:17 +0200

    fix(app): clean transitions

--- app/lib/settings/pages/about_us.dart
+++ app/lib/settings/pages/about_us.dart
@@ -5,20 +5,23 @@ class AboutUsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 16),
-            child: Text(
-              context.l10n.settings_page_about_us,
-              style: PharmeTheme.textTheme.headlineSmall,
+    return Container(
+      color: PharmeTheme.backgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              child: Text(
+                context.l10n.settings_page_about_us,
+                style: PharmeTheme.textTheme.headlineSmall,
+              ),
             ),
-          ),
-          Text(context.l10n.settings_page_about_us_text)
-        ],
+            Text(context.l10n.settings_page_about_us_text)
+          ],
+        ),
       ),
     );
   }
--- app/lib/settings/pages/terms_and_conditions.dart
+++ app/lib/settings/pages/terms_and_conditions.dart
@@ -5,20 +5,23 @@ class TermsAndConditionsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 16),
-            child: Text(
-              context.l10n.settings_page_terms_and_conditions,
-              style: PharmeTheme.textTheme.headlineSmall,
+    return Container(
+      color: PharmeTheme.backgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              child: Text(
+                context.l10n.settings_page_terms_and_conditions,
+                style: PharmeTheme.textTheme.headlineSmall,
+              ),
             ),
-          ),
-          Text(context.l10n.settings_page_terms_and_conditions_text)
-        ],
+            Text(context.l10n.settings_page_terms_and_conditions_text)
+          ],
+        ),
       ),
     );
   }
