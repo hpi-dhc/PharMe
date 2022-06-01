5bb41898f9c7c7d11675fd28d2546816c9d8da56
Samuel Kunst <samuel.kunst@gmail.com>
2022-06-01 16:12:42 +0200

    docs: add swagger docs for app controller

--- lab-server/src/app.controller.ts
+++ lab-server/src/app.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Unprotected } from 'nest-keycloak-connect';
 
 @Controller()
+@ApiTags('Server status')
 export class AppController {
-    @Unprotected()
     @Get('health')
+    @ApiOperation({ summary: 'Get the health of the server' })
+    @ApiResponse({
+        status: 200,
+        description: 'The service is running.',
+        content: { 'application/json': { example: 'ok' } },
+    })
+    @Unprotected()
     getHealth(): string {
         return 'ok';
     }
