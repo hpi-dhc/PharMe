0800691f5e98acd4fbd0a4cd45ee499f171fe1c0
Jannis Baum <jannis.baum@gmail.com>
2022-06-06 13:51:44 +0200

    refactor(anni): clean up initial next app

--- annotation-interface/pages/_app.tsx
+++ annotation-interface/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function AnnotationInterface({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>PharMe: Annotation Interface</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default AnnotationInterface;
