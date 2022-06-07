d7b28a462eba19cced3719b135c66a50f44b6a95
DevSchmidtchen <contact@schmidtchen.me>
2022-06-07 11:00:06 +0200

    feat: add supabase config

--- app/lib/common/constants.dart
+++ app/lib/common/constants.dart
@@ -1,3 +1,5 @@
+import 'package:comprehension_measurement/comprehension_measurement.dart';
+
 final labServerIp = Uri.https('lab-server-pharme.dhc-lab.hpi.de', '');
 final annotationServerIp = Uri.https(
   'annotation-server-pharme.dhc-lab.hpi.de',
@@ -9,3 +11,7 @@ final keycloakUrl = labServerIp;
 final cpicMaxCacheTime = Duration(days: 90);
 const cpicLookupUrl =
     'https://api.cpicpgx.org/v1/diplotype?select=genesymbol,diplotype,generesult';
+final supabaseConfig = SupabaseConfig(
+  'https://xrnczlpeghrewcseaxyq.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhybmN6bHBlZ2hyZXdjc2VheHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI5NjIzNTksImV4cCI6MTk2ODUzODM1OX0.3SPL2iHbDRS4m42n6UlOIuV8tFMShi7b9Mzh9l8E4Gs',
+);
