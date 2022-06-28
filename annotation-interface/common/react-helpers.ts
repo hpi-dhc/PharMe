62e3830744d5411e08ddb0b176e3b70a7ec23149
Jannis Baum <jannis.baum@gmail.com>
2022-06-28 17:06:24 +0200

    refactor(anni) generic useSrwFetcher

--- annotation-interface/common/react-helpers.ts
+++ annotation-interface/common/react-helpers.ts
@@ -1,4 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
 import { EffectCallback, useEffect } from 'react';
+import useSWR, { SWRResponse } from 'swr';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function useSwrFetcher<T>(
+    key: string[] | string,
+): SWRResponse<AxiosResponse<T, any>, any> {
+    return useSWR(
+        key,
+        async (url: string, params?: string) =>
+            await axios.get<T>(url, { params }),
+    );
+}
 
 /* eslint-disable react-hooks/exhaustive-deps */
 /* see comments on https://stackoverflow.com/a/56767883
