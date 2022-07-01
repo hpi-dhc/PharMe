06f580e4f8a99007c6398cc750dc911a3d4ffb32
Benjamin Frost <bfrost.schule@gmail.com>
2022-07-01 12:31:16 +0200

    feat(demo-app): add login screen

--- miscellaneous/demo-app/lib/login/pages/cubit.dart
+++ miscellaneous/demo-app/lib/login/pages/cubit.dart
@@ -1,70 +1,26 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:openid_client/openid_client_io.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../../common/module.dart';
-import '../models/lab.dart';
 
 part 'cubit.freezed.dart';
 
 class LoginPageCubit extends Cubit<LoginPageState> {
   LoginPageCubit() : super(LoginPageState.initial());
 
-  void revertToInitialState() => emit(LoginPageState.initial());
+  Future<void> fakeLoadGeneticData() async {
+    emit(LoginPageState.loading());
 
-  // signInAndLoadUserData authenticates a user with a Lab and fetches their
-  // genomic data from it's endpoint.
-  Future<void> signInAndLoadUserData(BuildContext context, Lab lab) async {
-    try {
-      // authenticate
-      final token = await _getAccessToken(context, lab.authUrl);
-      emit(LoginPageState.loadingUserData());
+    MetaData.instance.isLoggedIn = true;
+    await MetaData.save();
+    await Future.delayed(Duration(seconds: 3));
 
-      // get data
-      await fetchAndSaveDiplotypes(token, lab.endpoint);
-      await fetchAndSaveLookups();
-
-      // login + fetching of data successful
-      MetaData.instance.isLoggedIn = true;
-      await MetaData.save();
-      emit(LoginPageState.loadedUserData());
-    } catch (e) {
-      emit(LoginPageState.error(context.l10n.err_fetch_user_data_failed));
-    }
-  }
-
-  Future<String> _getAccessToken(BuildContext context, String authUrl) async {
-    final uri = Uri.parse(authUrl);
-    const clientId = 'pharme-app';
-    final scopes = List<String>.of(['openid', 'profile']);
-    const port = 4200;
-
-    final issuer = await Issuer.discover(uri);
-    final client = Client(issuer, clientId);
-
-    final authenticator = Authenticator(
-      client,
-      scopes: scopes,
-      port: port,
-      urlLancher: (url) async {
-        if (await canLaunchUrlString(url)) {
-          await launchUrlString(url);
-        } else {
-          throw Exception(context.l10n.err_could_not_launch(url));
-        }
-      },
-    );
-    final credentials = await authenticator.authorize();
-    await closeInAppWebView();
-    return credentials.getTokenResponse().then((res) => res.accessToken ?? '');
+    emit(LoginPageState.loaded());
   }
 }
 
 @freezed
 class LoginPageState with _$LoginPageState {
   const factory LoginPageState.initial() = _InitialState;
-  const factory LoginPageState.loadingUserData() = _LoadingUserDataState;
-  const factory LoginPageState.loadedUserData() = _LoadedUserDataState;
-  const factory LoginPageState.error(String string) = _ErrorState;
+  const factory LoginPageState.loading() = _LoadingState;
+  const factory LoginPageState.loaded() = _LoadedState;
 }
--- miscellaneous/demo-app/lib/login/pages/login.dart
+++ miscellaneous/demo-app/lib/login/pages/login.dart
@@ -1,7 +1,4 @@
-import 'package:dropdown_button2/dropdown_button2.dart';
-
 import '../../../common/module.dart';
-import '../models/lab.dart';
 import 'cubit.dart';
 
 class LoginPage extends StatefulWidget {
@@ -21,21 +18,16 @@ class _LoginPageState extends State<LoginPage> {
       child: BlocBuilder<LoginPageCubit, LoginPageState>(
         builder: (context, state) {
           return Scaffold(
-            body: Padding(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SvgPicture.asset('assets/images/pharme_logo_horizontal.svg'),
-                  Column(
-                    children: state.when(
-                      initial: () => _buildInitialScreen(context),
-                      loadingUserData: () => _buildLoadingScreen(context),
-                      loadedUserData: () => _buildLoadedScreen(context),
-                      error: (message) => _buildErrorScreen(context, message),
-                    ),
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: SizedBox.expand(
+                  child: state.when(
+                    initial: () => _buildInitialScreen(context),
+                    loading: () => _buildLoadingScreen(context),
+                    loaded: () => _buildLoadedScreen(context),
                   ),
-                ],
+                ),
               ),
             ),
           );
@@ -44,123 +36,109 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  List<Widget> _buildInitialScreen(BuildContext context) {
-    return [
-      DropdownButtonHideUnderline(
-        child: DropdownButton2(
-          isExpanded: true,
-          hint: Text(context.l10n.auth_choose_lab),
-          value: dropdownValue,
-          onChanged: (value) {
-            setState(() {
-              dropdownValue = value.toString();
-            });
-          },
-          items: labs
-              .map((lab) => DropdownMenuItem(
-                    value: lab.name,
-                    child: Text(lab.name),
-                  ))
-              .toList(),
-          buttonPadding: const EdgeInsets.only(left: 16, right: 16),
-          buttonDecoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(32),
-            border: Border.all(color: Colors.black26),
-          ),
-          dropdownDecoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-          ),
+  Widget _buildInitialScreen(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SvgPicture.asset(
+          'assets/images/pharme_logo_horizontal.svg',
         ),
-      ),
-      SizedBox(height: 8),
-      SizedBox(
-        width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () async {
-            final found = labs.firstWhere((el) => el.name == dropdownValue);
-            await context
-                .read<LoginPageCubit>()
-                .signInAndLoadUserData(context, found);
-          },
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(32),
+        Column(
+          children: [
+            Icon(
+              Icons.lock_open,
+              color: Colors.green,
+              size: 128,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'You are already logged in',
+              style: context.textTheme.headline5,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: context.read<LoginPageCubit>().fakeLoadGeneticData,
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
               ),
             ),
+            child: Text('Load my genetic data'),
           ),
-          child: Text(context.l10n.auth_sign_in),
         ),
-      )
-    ];
+      ],
+    );
   }
 
-  List<Widget> _buildLoadingScreen(BuildContext context) {
-    return [
-      SizedBox(
-        width: 200,
-        height: 200,
-        child: RadiantGradientMask(
-          colors: [
-            PharmeTheme.primaryContainer,
-            PharmeTheme.secondaryContainer,
+  Widget _buildLoadingScreen(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SvgPicture.asset(
+          'assets/images/pharme_logo_horizontal.svg',
+        ),
+        CircularProgressIndicator(),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: null, // disabled state
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+              ),
+            ),
+            child: Text('Load my genetic data'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoadedScreen(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SvgPicture.asset(
+          'assets/images/pharme_logo_horizontal.svg',
+        ),
+        Column(
+          children: [
+            Icon(
+              Icons.check_circle_outline,
+              color: Colors.green,
+              size: 128,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Data loaded successfully',
+              style: context.textTheme.headline6,
+              textAlign: TextAlign.center,
+            ),
           ],
-          child: CircularProgressIndicator(
-            color: Colors.white,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () => context.router.replace(MainRoute()),
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+              ),
+            ),
+            child: Text('Continue to PharMe'),
           ),
         ),
-      ),
-    ];
-  }
-
-  List<Widget> _buildLoadedScreen(BuildContext context) {
-    return [
-      RadiantGradientMask(
-        colors: [
-          PharmeTheme.primaryContainer,
-          PharmeTheme.secondaryContainer,
-        ],
-        child: Icon(
-          Icons.task_alt,
-          size: 152,
-          color: Colors.white,
-        ),
-      ),
-      Text(context.l10n.auth_success),
-      SizedBox(height: 24),
-      SizedBox(
-        width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () => context.router.replace(MainRoute()),
-          child: Text(context.l10n.general_continue),
-        ),
-      ),
-    ];
-  }
-
-  List<Widget> _buildErrorScreen(BuildContext context, String message) {
-    return [
-      RadiantGradientMask(
-        colors: [
-          PharmeTheme.primaryContainer,
-          PharmeTheme.secondaryContainer,
-        ],
-        child: Icon(
-          Icons.warning_amber_outlined,
-          size: 152,
-          color: Colors.white,
-        ),
-      ),
-      Text(message),
-      SizedBox(height: 24),
-      SizedBox(
-        width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () =>
-              context.read<LoginPageCubit>().revertToInitialState(),
-          child: Text(context.l10n.general_retry),
-        ),
-      ),
-    ];
+      ],
+    );
   }
 }
