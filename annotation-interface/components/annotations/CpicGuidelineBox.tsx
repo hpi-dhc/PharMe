dee0a4b88c93925ae281397c2ce28c8c1b77abd3
Jannis Baum <jannis.baum@gmail.com>
2022-07-03 19:15:39 +0200

    fix(anni): cpic guideline box showing curated data

--- annotation-interface/components/annotations/CpicGuidelineBox.tsx
+++ annotation-interface/components/annotations/CpicGuidelineBox.tsx
@@ -15,37 +15,33 @@ const CpicSection = ({
     </p>
 );
 
-const CpicGuidelineBox = ({ guideline }: { guideline: ServerGuideline }) => {
-    const fields: [string, string | null][] = [
-        ['Comment', guideline.cpicComment],
-        [
-            'Phenotype consultation text',
-            guideline.phenotype.cpicConsultationText,
-        ],
-        ['Implication', guideline.implication],
-        ['Recommendation', guideline.recommendation],
-    ];
-    return (
-        <div className="space-y-4 border border-black border-opacity-10 p-4">
-            <h2 className="font-bold pb-2">
-                CPIC Guideline:{' '}
-                <a
-                    className="underline"
-                    href={guideline.cpicGuidelineUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    {guideline.cpicGuidelineName}
-                </a>{' '}
-                ({guideline.cpicClassification})
-            </h2>
-            {fields
-                .filter(([, content]) => content)
-                .map(([title, content], index) => (
-                    <CpicSection key={index} title={title} content={content!} />
-                ))}
-        </div>
-    );
-};
+const fields = (guideline: ServerGuideline): [string, string | null][] => [
+    ['Comment', guideline.cpicComment],
+    ['Phenotype consultation text', guideline.phenotype.cpicConsultationText],
+    ['Implication', guideline.cpicImplication],
+    ['Recommendation', guideline.cpicRecommendation],
+];
+
+const CpicGuidelineBox = ({ guideline }: { guideline: ServerGuideline }) => (
+    <div className="space-y-4 border border-black border-opacity-10 p-4">
+        <h2 className="font-bold pb-2">
+            CPIC Guideline:{' '}
+            <a
+                className="underline"
+                href={guideline.cpicGuidelineUrl}
+                target="_blank"
+                rel="noreferrer"
+            >
+                {guideline.cpicGuidelineName}
+            </a>{' '}
+            ({guideline.cpicClassification})
+        </h2>
+        {fields(guideline)
+            .filter(([, content]) => content)
+            .map(([title, content], index) => (
+                <CpicSection key={index} title={title} content={content!} />
+            ))}
+    </div>
+);
 
 export default CpicGuidelineBox;
