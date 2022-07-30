b2b1b4f4b4e33177f2ec7319ac4a3cb2de4d2b7d
Nikola Genchev <n.genchev@hotmail.com>
2022-07-30 16:50:25 +0200

    test(app): fix search tests

--- app/integration_test/search_test.dart
+++ app/integration_test/search_test.dart
@@ -1,47 +1,68 @@
 import 'package:app/common/module.dart';
-import 'package:app/search/pages/cubit.dart';
+import 'package:app/search/module.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 class MockSearchCubit extends MockCubit<SearchState> implements SearchCubit {}
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final mockSearchCubit = MockSearchCubit();
 
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.onlyPumps;
-
-  late SearchCubit searchCubit;
-
-  setUp(() {
-    searchCubit = MockSearchCubit();
-  });
-
-  group('integration test for the faq page', () {
-    final appRouter = AppRouter();
-    final searchWidget = MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routeInformationParser: appRouter.defaultRouteParser(),
-      routerDelegate: appRouter.delegate(
-        initialDeepLink: 'main/search',
-      ),
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [Locale('en', '')],
-    );
-
-    testWidgets('test search page', (tester) async {
+  final loadedMedications = [
+    Medication(1, 'Codeine', 'test description', 'test class', 'test'),
+    Medication(2, 'Clopidogrel', 'test description', 'test class', 'test'),
+    Medication(3, 'Ibuprofen', 'test description', 'test class', 'test'),
+  ];
+  group('integration test for the search page', () {
+    testWidgets('test search page in loading state', (tester) async {
+      when(() => mockSearchCubit.state).thenReturn(SearchState.loading());
       await tester.pumpWidget(BlocProvider.value(
-        value: searchCubit,
-        child: searchWidget,
+        value: mockSearchCubit,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SearchPage(cubit: mockSearchCubit),
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [Locale('en', '')],
+        ),
+      ));
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+
+    testWidgets('test search page in loaded state', (tester) async {
+      when(() => mockSearchCubit.state)
+          .thenReturn(SearchState.loaded(loadedMedications));
+
+      await tester.pumpWidget(BlocProvider.value(
+        value: mockSearchCubit,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SearchPage(cubit: mockSearchCubit),
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [Locale('en', '')],
+        ),
       ));
       await tester.pumpAndSettle();
-      print('5');
+
+      expect(
+        find.byType(MedicationCard),
+        findsNWidgets(loadedMedications.length),
+      );
     });
   });
 }
