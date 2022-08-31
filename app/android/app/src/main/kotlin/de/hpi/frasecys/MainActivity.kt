b627a54af553c6b18baad2af72c5e7795a700f85
Thomas Harris <teh019283@gmail.com>
2022-08-31 11:25:48 +0200

    Log out and log in work

--- app/android/app/src/main/kotlin/de/hpi/frasecys/MainActivity.kt
+++ app/android/app/src/main/kotlin/de/hpi/frasecys/MainActivity.kt
@@ -6,6 +6,8 @@ import care.data4life.sdk.Data4LifeClient
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import android.os.Bundle
+import care.data4life.sdk.listener.ResultListener
+import care.data4life.sdk.lang.D4LException
 
 class MainActivity : FlutterActivity() {
 
@@ -23,9 +25,68 @@ class MainActivity : FlutterActivity() {
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "chdp")
             .setMethodCallHandler { call, result ->
                 when (call.method) {
-                    "isLoggedIn" -> result.success(true)
+                    "isLoggedIn" -> Data4LifeClient.getInstance().isUserLoggedIn(
+                        object : ResultListener<Boolean> {
+                            override fun onSuccess(t: Boolean) = result.success(t)
+
+                            override fun onError(exception: D4LException) =
+                                result.error(
+                                    "D4LException",
+                                    "Failed to check login status",
+                                    exception
+                                )
+                        }
+                    )
+                    "login" -> {
+                        val intent = Data4LifeClient.getInstance().getLoginIntent(
+                            this@MainActivity,
+                            setOf(
+                                "perm:r",
+                                "rec:r",
+                                "rec:w",
+                                "attachment:r",
+                                "attachment:w",
+                                "user:r",
+                                "user:w",
+                                "user:q",
+                            )
+                        )
+
+                        this@MainActivity.startActivityForResult(intent, Data4LifeClient.D4L_AUTH)
+
+                        result.success(0)
+                    }
+                    "logout" -> {
+                        Data4LifeClient.getInstance().logout(
+                            object : care.data4life.sdk.listener.Callback {
+                                override fun onSuccess() = result.success(0)
+
+                                override fun onError(exception: D4LException) =
+                                    result.error(
+                                        "D4LException",
+                                        "Failed to logout",
+                                        exception
+                                    )
+                            }
+                        )
+
+                    }
                     else -> result.notImplemented()
                 }
             }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: android.content.Intent?,
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Data4LifeClient.D4L_AUTH) {
+            if (resultCode == android.app.Activity.RESULT_OK) {
+                android.util.Log.i("PHARME", "login successful")
+            }
+        }
     }
 }
--- app/lib/settings/chdp_state.dart
+++ app/lib/settings/chdp_state.dart
@@ -9,7 +9,7 @@ class ChdpListTile extends StatefulWidget {
   State<ChdpListTile> createState() => _ChdpState();
 }
 
-class _ChdpState extends State<ChdpListTile> {
+class _ChdpState extends State<ChdpListTile> with WidgetsBindingObserver {
   static const platform = MethodChannel('chdp');
 
   bool _isLoggedIn = false;
@@ -17,14 +17,40 @@ class _ChdpState extends State<ChdpListTile> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     getIsLoggedIn().whenComplete(() => null);
   }
 
   @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      getIsLoggedIn().whenComplete(() => null);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final title = _isLoggedIn
+        ? Text('Log out of Smart4Health')
+        : Text('Log in to Smart4Health');
+
     return ListTile(
-      title: Text('Is logged in? $_isLoggedIn'),
-    );
+      title: title,
+      trailing: _isLoggedIn ? null : Icon(Icons.chevron_right),
+      onTap: () => {
+        // are you okay intellij autoformatter????
+        if (_isLoggedIn)
+          {logout().whenComplete(getIsLoggedIn)}
+        else
+          {login().whenComplete(() => null)}
+      },
+    ); // ListTile
   }
 
   Future<void> getIsLoggedIn() async {
@@ -38,5 +64,21 @@ class _ChdpState extends State<ChdpListTile> {
     setState(() {
       _isLoggedIn = isLoggedIn;
     });
+  }
+
+  Future<void> login() async {
+    try {
+      await platform.invokeMethod('login');
+    } on PlatformException catch (e) {
+      debugPrint('platform exception in login');
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      await platform.invokeMethod('logout');
+    } on PlatformException catch (e) {
+      debugPrint('platform exception in logout');
+    }
   }
 }
