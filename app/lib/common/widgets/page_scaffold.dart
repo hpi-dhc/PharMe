cad629f1446d13054bda007d0311aa2c4ed245c8
Jannis Baum <jannis.baum@gmail.com>
2022-10-21 13:48:02 +0200

    feat: support actions in scaffold

--- app/lib/common/widgets/page_scaffold.dart
+++ app/lib/common/widgets/page_scaffold.dart
@@ -1,17 +1,23 @@
 import '../module.dart';
 
 Scaffold pageScaffold(
-    {required String title, Widget? barBottom, required List<Widget> body}) {
+    {required String title,
+    Widget? barBottom,
+    List<Widget>? actions,
+    required List<Widget> body}) {
   return Scaffold(
     body: CustomScrollView(slivers: [
       SliverAppBar(
         backgroundColor: PharMeTheme.surfaceColor,
+        foregroundColor: PharMeTheme.onSurfaceText,
         elevation: 0,
+        leadingWidth: 24,
         floating: true,
         pinned: true,
         snap: false,
         centerTitle: false,
         title: Text(title, style: PharMeTheme.textTheme.headlineLarge),
+        actions: actions,
         bottom: barBottom == null
             ? null
             : AppBar(
