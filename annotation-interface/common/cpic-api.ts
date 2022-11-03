8243b013417eca707c1fc3dcabf1f6b8cf22d8e4
Jannis Baum <jannis.baum@gmail.com>
2022-11-03 14:59:17 +0100

    feat(#487): add cpic api constants

---
+++ annotation-interface/common/cpic-api.ts
@@ -0,0 +1,20 @@
+export type CpicRecommendation = {
+    id: number;
+    drugid: string;
+    version: number;
+
+    drug: { name: string };
+    lookupkey: { [key: string]: string }; // gene-symbol: phenotype
+
+    guideline: { name: string; url: string };
+    classification: string;
+    implications: { [key: string]: string }; // gene-symbol: implication
+    drugrecommendation: string;
+    comments?: string;
+};
+
+export const cpicRecommendationsURL =
+    'https://api.cpicpgx.org/v1/recommendation';
+export const cpicRecommendationsParams = {
+    select: 'id,drugid,version,drug(name),lookupkey,guideline(name,url),classification,implications,drugrecommendation,comments',
+};
