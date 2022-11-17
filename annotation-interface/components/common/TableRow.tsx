c4df0f6ba77ab004058fa897a7fc376a878f47ac
Jannis Baum <jannis.baum@gmail.com>
2022-11-17 19:06:27 +0100

    fix(anni): remove explicit key prop

--- annotation-interface/components/common/TableRow.tsx
+++ annotation-interface/components/common/TableRow.tsx
@@ -1,17 +1,16 @@
 import { ChevronRightIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
-import { Key, PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 
 import WithIcon from './WithIcon';
 
 interface Props {
-    key?: Key;
     link: string;
 }
 
-function TableRow({ key, link, children }: PropsWithChildren<Props>) {
+function TableRow({ link, children }: PropsWithChildren<Props>) {
     return (
-        <Link key={key} href={link}>
+        <Link href={link}>
             <a className="border-t border-black border-opacity-10 py-3 pl-3 flex justify-between hover:bg-neutral-50">
                 <div className="grow">{children}</div>
                 <WithIcon icon={ChevronRightIcon} reverse className="px-2" />
