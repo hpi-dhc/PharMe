c326c43d4b8a113419eea307477b62a57b8dd86e
Jannis Baum <jannis.baum@gmail.com>
2022-11-18 09:46:53 +0100

    feat(anni): clear search

--- annotation-interface/components/common/SearchBar.tsx
+++ annotation-interface/components/common/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, XIcon } from '@heroicons/react/outline';
 
 type Props = {
     query: string;
@@ -30,6 +30,14 @@ const SearchBar = ({ query, setQuery, placeholder, dark, onEnter }: Props) => {
                 }}
                 autoFocus
             />
+            {!!query && (
+                <button
+                    className="rounded-full bg-red"
+                    onClick={() => setQuery('')}
+                >
+                    <XIcon className="w-4 h-4 absolute top-1/2 transform -translate-y-1/2 right-2 opacity-60" />
+                </button>
+            )}
         </div>
     );
 };
