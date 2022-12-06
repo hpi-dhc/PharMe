eaf115acb5775c629c5ecf40d37f2d2c3b50a164
Jannis Baum <jannis.baum@gmail.com>
2022-12-06 17:34:41 +0100

    feat(#497): staging PATCH api

--- annotation-interface/pages/api/annotations/staging/[id].ts
+++ annotation-interface/pages/api/annotations/staging/[id].ts
@@ -1,38 +1,62 @@
 import { NextApiHandler } from 'next';
 import { ApiError } from 'next/dist/server/api-utils';
 
-import { handleApiMethods } from '../../../../common/api-helpers';
+import { ApiResponse, handleApiMethods } from '../../../../common/api-helpers';
 import dbConnect from '../../../../database/helpers/connect';
-import { allAnnotationModels } from '../[id]';
+import { allAnnotationModels, AnyAnnotationModel } from '../[id]';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-const modelAndDoc = async (id: string) => {
+interface ResponseData {
+    isStaged: boolean;
+}
+export type GetStagingResponse = ApiResponse<ResponseData>;
+
+export interface UpdateStagingBody {
+    isStaged: 'true' | 'false';
+}
+
+async function execOrFail<T>(
+    f: (model: AnyAnnotationModel) => Promise<T>,
+): Promise<T> {
     await dbConnect();
     const results = await Promise.all(
-        allAnnotationModels.map(
-            async (model): Promise<[any, any] | undefined> => {
-                const doc = await (model as any).findById(id).lean().exec();
-                if (doc) return [model, doc];
-                return undefined;
-            },
-        ),
+        allAnnotationModels.map(async (model): Promise<T | undefined> => {
+            const result = await f(model);
+            if (result) return result;
+            return undefined;
+        }),
     );
     const ret = results.find((result) => result);
-    if (!ret)
-        throw new ApiError(404, `Annotation Document with id ${id} not found`);
+    if (!ret) throw new ApiError(404, 'Annotation document not found.');
     return ret;
-};
+}
 
 const api: NextApiHandler = async (req, res) => {
     const {
         query: { id },
     } = req;
 
-    return await handleApiMethods(req, res, {
+    await handleApiMethods(req, res, {
         GET: async () => {
-            const [, doc] = await modelAndDoc(id as string);
-            return { successStatus: 200, data: { isStaged: doc.isStaged } };
+            const doc = await execOrFail(
+                async (model) =>
+                    await (model as any).findById(id).lean().exec(),
+            );
+            const data: ResponseData = {
+                isStaged: doc.isStaged,
+            };
+            return { successStatus: 200, data };
+        },
+        PATCH: async () => {
+            const { isStaged } = req.body as UpdateStagingBody;
+            await execOrFail(
+                async (model) =>
+                    await (model as any).findByIdAndUpdate(id, {
+                        isStaged: isStaged === 'true' ? true : false,
+                    }),
+            );
+            return { successStatus: 200 };
         },
     });
 };
