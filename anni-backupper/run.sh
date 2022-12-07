c45c11dbcaae02dba8650515f2cdc4efa963904d
Jannis Baum <jannis.baum@gmail.com>
2022-12-07 17:01:22 +0100

    fix(#498): set repo dir correctly

--- anni-backupper/run.sh
+++ anni-backupper/run.sh
@@ -4,7 +4,7 @@ ROOT_DIR=$(dirname $(realpath $0))
 
 . $ROOT_DIR/.env
 
-test -d $BACKUP_DIR || git clone https://oauth2:$GITHUB_OAUTH@github.com/hpi-dhc/PharMe-Data
+test -d $BACKUP_DIR || git clone https://oauth2:$GITHUB_OAUTH@github.com/hpi-dhc/PharMe-Data $BACKUP_DIR
 
 curl -s $ANNI_URL/api/backup | jq '.data' > $BACKUP_DIR/backup.json
 
