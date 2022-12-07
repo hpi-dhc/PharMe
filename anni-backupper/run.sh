8bb333ea0732a45af8173234b37416caa717a13c
Jannis Baum <jannis.baum@gmail.com>
2022-12-07 16:55:48 +0100

    feat(#498): customize backup dir

--- anni-backupper/run.sh
+++ anni-backupper/run.sh
@@ -4,11 +4,10 @@ ROOT_DIR=$(dirname $(realpath $0))
 
 . $ROOT_DIR/.env
 
-BACKUP_DIR=$ROOT_DIR/backups
 test -d $BACKUP_DIR || git clone https://oauth2:$GITHUB_OAUTH@github.com/hpi-dhc/PharMe-Data
 
 curl -s $ANNI_URL/api/backup | jq '.data' > $BACKUP_DIR/backup.json
 
 git -C $BACKUP_DIR add --all
-git -C $BACKUP_DIR commit --message="$(date '+%y-%m-%d')"
+git -C $BACKUP_DIR commit --message="Backup from $(date +%y-%m-%d) at $(date +%T) ($(date +%Z))"
 git -C $BACKUP_DIR push
