012d1228d6fc645f19b25bb58e3edb7ca37234d9
Jannis Baum <jannis.baum@gmail.com>
2023-01-03 15:50:54 +0100

    chore(#525): remove annotation server

--- annotation-server/src/phenotypes/entities/gene-result.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, Column } from 'typeorm';
-
-import { BaseEntity } from '../../common/entities/base.entity';
-
-@Entity()
-export class GeneResult extends BaseEntity {
-    @Column()
-    name: string;
-}
