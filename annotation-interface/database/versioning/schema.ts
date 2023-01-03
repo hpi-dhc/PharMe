03ef8d8a36347499cca9a0e2d6c445dff5fdf86e
Jannis Baum <jannis.baum@gmail.com>
2023-01-03 16:23:52 +0100

    refactor(#513): define methods in schema options

--- annotation-interface/database/versioning/schema.ts
+++ annotation-interface/database/versioning/schema.ts
@@ -9,14 +9,14 @@ import mongoose, {
 import { IBaseDoc, MongooseId } from '../helpers/types';
 
 type IVersionedDoc<DocT extends IBaseDoc<Types.ObjectId>> = DocT & {
-    _v?: number;
-    _vDate?: Date;
-    findHistoryDoc?: () => Promise<IVersionHistoryDoc<DocT>>;
+    _v: number;
+    _vDate: Date;
+    findHistoryDoc: () => Promise<IVersionHistoryDoc<DocT>>;
 };
 
 type IVersionHistoryDoc<DocT extends IBaseDoc<Types.ObjectId>> =
     IVersionedDoc<DocT> & {
-        _ref?: Types.ObjectId;
+        _ref: Types.ObjectId;
     };
 
 type VersionedModel<DocT, HDocT> = Model<DocT> & {
@@ -42,21 +42,27 @@ export function versionedModel<DocT extends IBaseDoc<Types.ObjectId>>(
         _v: { type: Number, required: true },
         _vDate: { type: Date, required: true },
     };
-    const schema = new mongoose.Schema<VD, VM>(schemaDefinition);
-    schema.static('findVersions', async function (id: MongooseId): Promise<
-        Array<VHD>
-    > {
-        return historyModel.find({ _ref: id });
-    });
-    schema.static(
-        'findOneVersion',
-        async function (id: MongooseId, v: number): Promise<VHD | null> {
-            return historyModel.findOne({ _ref: id, _v: v });
+    const schema = new mongoose.Schema<VD, VM>(schemaDefinition, {
+        methods: {
+            findHistoryDoc: async function (this: VD) {
+                return historyModel.findOne({
+                    _ref: this._id,
+                    _v: this._v,
+                });
+            },
         },
-    );
-    schema.methods.findHistoryDoc = async function (this: VD) {
-        return historyModel.findOne({ _ref: this._id, _v: this._v });
-    };
+        statics: {
+            findVersions: async function (id: MongooseId): Promise<Array<VHD>> {
+                return historyModel.find({ _ref: id });
+            },
+            findOneVersion: async function (
+                id: MongooseId,
+                v: number,
+            ): Promise<VHD | null> {
+                return historyModel.findOne({ _ref: id, _v: v });
+            },
+        },
+    });
     // versioning middleware
     // save first version & init version number
     schema.pre('validate', async function (this: VD) {
@@ -87,21 +93,27 @@ export function versionedModel<DocT extends IBaseDoc<Types.ObjectId>>(
         mongoose.model<VD, VM>(modelName, schema);
 
     // version history model ----------------------------------------------------
-    const historySchema = new mongoose.Schema<VHD, VHM>({
-        ...schemaDefinition,
-        _ref: { type: Types.ObjectId, ref: modelName, required: true },
-    });
+    const historySchema = new mongoose.Schema<VHD, VHM>(
+        {
+            ...schemaDefinition,
+            _ref: { type: Types.ObjectId, ref: modelName, required: true },
+        },
+        {
+            statics: {
+                saveVersion: async function (document: VD) {
+                    const historyDoc: VHD = {
+                        ...JSON.parse(JSON.stringify(document)),
+                        _ref: document._id!,
+                        _id: undefined,
+                    };
+                    await historyModel.create(historyDoc);
+                },
+            },
+        },
+    );
     historySchema.index({ _ref: 'hashed', _v: 1 }, { unique: true });
     historySchema.index({ _ref: 'hashed', _vDate: 1 });
     historySchema.index({ _ref: 'hashed' });
-    historySchema.static('saveVersion', async function (document: VD) {
-        const historyDoc: VHD = {
-            ...JSON.parse(JSON.stringify(document)),
-            _ref: document._id!,
-            _id: undefined,
-        };
-        await historyModel.create(historyDoc);
-    });
     const hModelName = `${modelName}_History`;
     const historyModel =
         (mongoose.models[hModelName] as VHM) ||
