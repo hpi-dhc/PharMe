0d32af5e019daaaf624b40deed0216704370b084
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 21:08:16 +0100

    feat(#518): add publishing overlay

--- anni/src/pages/index.tsx
+++ anni/src/pages/index.tsx
@@ -1,19 +1,32 @@
-import { CheckIcon, ExclamationIcon } from '@heroicons/react/solid';
+import {
+    CheckIcon,
+    ExclamationIcon,
+    UploadIcon,
+    XIcon,
+} from '@heroicons/react/solid';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { useSwrFetcher } from '../common/react-helpers';
 import WithIcon from '../components/common/WithIcon';
 import GenericError from '../components/common/indicators/GenericError';
 import LoadingSpinner from '../components/common/indicators/LoadingSpinner';
+import Button from '../components/common/interaction/Button';
 import PageHeading from '../components/common/structure/PageHeading';
+import PageOverlay from '../components/common/structure/PageOverlay';
 import Emphasis from '../components/common/text/Emphasis';
 import Explanation from '../components/common/text/Explanation';
 import { GetPublishStatusReponse } from './api/publish';
 
 const Home = () => {
+    const [publishVisible, setPublishVisible] = useState(false);
     const { data: response, error } =
         useSwrFetcher<GetPublishStatusReponse>('/api/publish');
     const publishingError = response?.data.data.errorMessage;
+
+    const publish = async () => {
+        console.log('publish');
+    };
 
     return (
         <div className="space-y-4">
@@ -45,16 +58,25 @@ const Home = () => {
                     </p>
                 </Explanation>
                 <h3 className="font-bold">Current status</h3>
-                <div className="flex justify-between">
+                <div className="flex justify-between items-center">
                     {response ? (
                         publishingError ? (
                             <WithIcon icon={ExclamationIcon} as="p">
                                 {publishingError}
                             </WithIcon>
                         ) : (
-                            <WithIcon icon={CheckIcon} as="p">
-                                Ready to publish.
-                            </WithIcon>
+                            <>
+                                <WithIcon icon={CheckIcon} as="p">
+                                    Ready to publish.
+                                </WithIcon>
+                                <Button
+                                    icon={UploadIcon}
+                                    onClick={() => setPublishVisible(true)}
+                                    reverse
+                                >
+                                    Publish
+                                </Button>
+                            </>
                         )
                     ) : error ? (
                         <GenericError />
@@ -63,6 +85,26 @@ const Home = () => {
                     )}
                 </div>
             </div>
+            {publishVisible && (
+                <PageOverlay
+                    hide={() => setPublishVisible(false)}
+                    heading="Publish data"
+                    explanation="Are you sure? This action will have an immediate effect on users and cannot be directly undone."
+                >
+                    <div className="flex justify-center space-x-4">
+                        <Button onClick={publish} icon={UploadIcon} dark>
+                            Publish now
+                        </Button>
+                        <Button
+                            onClick={() => setPublishVisible(false)}
+                            icon={XIcon}
+                            dark
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </PageOverlay>
+            )}
         </div>
     );
 };
