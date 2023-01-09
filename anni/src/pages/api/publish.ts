ec806d54827f1e6e17e37fc021c7c5f6b3b55076
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 18:26:27 +0100

    refactor(#518): publish api with post

--- anni/src/pages/api/publish.ts
+++ anni/src/pages/api/publish.ts
@@ -1,31 +1,21 @@
 import { NextApiHandler } from 'next';
-import { ApiError } from 'next/dist/server/api-utils';
 
 import { handleApiMethods } from '../../common/api-helpers';
-import {
-    SupportedLanguage,
-    supportedLanguages,
-} from '../../common/definitions';
+import { pharMeLanguage } from '../../common/definitions';
 import dbConnect from '../../database/helpers/connect';
+import AppData from '../../database/models/AppData';
 import Drug from '../../database/models/Drug';
 
 const api: NextApiHandler = async (req, res) =>
     await handleApiMethods(req, res, {
-        GET: async () => {
-            const language = req.query.language as SupportedLanguage;
-            if (!(language && supportedLanguages.includes(language))) {
-                throw new ApiError(400, 'Language not specified.');
-            }
-
+        POST: async () => {
             await dbConnect();
             const drugs = await Drug!.find({ isStaged: true }).exec();
             const resolved = await Promise.all(
-                drugs.map((drug) => drug.resolve(language)),
+                drugs.map((drug) => drug.resolve(pharMeLanguage)),
             );
-            return {
-                successStatus: 200,
-                data: resolved,
-            };
+            await AppData!.publish({ drugs: resolved });
+            return { successStatus: 201 };
         },
     });
 
