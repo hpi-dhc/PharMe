ad23c65ad95e2b812d80c76bc361d3f35c800e2f
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 17:47:29 +0100

    test(#518): singleton app data model

---
+++ anni/src/database/models/AppData.test.ts
@@ -0,0 +1,59 @@
+import dbConnect from '../helpers/connect';
+import AppData from './AppData';
+
+describe('App data', () => {
+    beforeAll(async () => {
+        await dbConnect();
+    });
+
+    describe('no data', () => {
+        it('should get null data', async () => {
+            const current = await AppData!.getCurrent();
+            expect(current).toBeNull();
+        });
+
+        it('should get null version', async () => {
+            const version = await AppData!.getVersion();
+            expect(version).toBeNull();
+        });
+    });
+
+    describe('save version 1', () => {
+        it('should save a document', async () => {
+            await AppData!.publish({ drugs: [] });
+        });
+    });
+
+    const validateDocAndVersion = (expectedVersion: number) => {
+        it('should get the current document', async () => {
+            const doc = await AppData!.getCurrent();
+            expect(doc).not.toBeNull();
+            expect(doc!.drugs).toEqual([]);
+        });
+
+        it('should get the current version', async () => {
+            const version = await AppData!.getVersion();
+            expect(version).not.toBeNull();
+            expect(version).toEqual(expectedVersion);
+        });
+
+        it('should have exactly one document', async () => {
+            const allDocs = await AppData!.find();
+            expect(allDocs.length).toEqual(1);
+        });
+    };
+
+    describe('get version 1', () => {
+        validateDocAndVersion(1);
+    });
+
+    describe('update version 1', () => {
+        it('should update the saved document', async () => {
+            await AppData!.publish({ drugs: [] });
+        });
+    });
+
+    describe('get version 2', () => {
+        validateDocAndVersion(2);
+    });
+});
