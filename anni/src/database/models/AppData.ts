222d2d177557b76c16fa5ccab9bd620e7860e651
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 17:47:16 +0100

    feat(#518): singleton app data model

---
+++ anni/src/database/models/AppData.ts
@@ -0,0 +1,56 @@
+import mongoose, { Types } from 'mongoose';
+
+import { IBaseDoc, MongooseId, OptionalId } from '../helpers/types';
+import { IVersionedDoc, versionedModel } from '../versioning/schema';
+import { IDrug_Any } from './Drug';
+
+export interface IAppData<
+    DrugT extends MongooseId | IDrug_Any,
+    IdT extends OptionalId = undefined,
+> extends IBaseDoc<IdT> {
+    drugs: DrugT[];
+}
+
+export type IAppData_DB = IAppData<Types.ObjectId, Types.ObjectId>;
+export type IAppData_Patch = Partial<
+    IAppData<MongooseId | IDrug_Any, undefined>
+>;
+
+const { schema, makeModel } = versionedModel<
+    IAppData_DB,
+    {
+        getCurrent: () => Promise<IVersionedDoc<IAppData_DB> | null>;
+        getVersion: () => Promise<number | null>;
+        publish: (data: IAppData_Patch) => Promise<IVersionedDoc<IAppData_DB>>;
+    }
+>('AppData', {
+    drugs: {
+        type: [{ type: Types.ObjectId, ref: 'Drug' }],
+        required: true,
+    },
+});
+
+schema.statics.getCurrent = function (this: ReturnType<typeof makeModel>) {
+    return this.findOne() ?? null;
+};
+
+schema.statics.getVersion = async function (
+    this: ReturnType<typeof makeModel>,
+) {
+    const current = await this.findOne();
+    return current?._v ?? null;
+};
+
+schema.statics.publish = async function (
+    this: ReturnType<typeof makeModel>,
+    data: IAppData_Patch,
+) {
+    try {
+        const doc = await this.findOneAndUpdate(undefined, data);
+        return doc;
+    } catch {
+        return this.create(data);
+    }
+};
+
+export default !mongoose.models ? undefined : makeModel();
