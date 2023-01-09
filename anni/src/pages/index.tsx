cf98432e63893df95b86abd6d66ad384b30336f9
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 19:55:34 +0100

    feat(#518): improve home intro text

--- anni/src/pages/index.tsx
+++ anni/src/pages/index.tsx
@@ -4,22 +4,24 @@ import PageHeading from '../components/common/structure/PageHeading';
 
 const Home = () => {
     return (
-        <>
+        <div className="space-y-4">
             <PageHeading title="PharMe's Annotation Interface">
                 Welcome to the curator&apos;s interface to PharMe&apos;s
-                user-agnostic data. PharMe uses{' '}
+                user-agnostic data: the Annotation Interface aka.{' '}
+                <span className="italic">Anni</span>! PharMe uses{' '}
                 <span className="italic">external data</span> from{' '}
                 <a className="underline" href="https://cpicpgx.org">
                     CPIC
                 </a>{' '}
-                as well as <span className="italic">internal data</span> defined
-                by{' '}
+                and supplements it with patient-oriented{' '}
+                <span className="italic">internal data</span>. This internal
+                data is defined using Anni and referred to as{' '}
                 <Link href="/annotations">
                     <a className="underline">Annotations</a>
                 </Link>
                 .
             </PageHeading>
-        </>
+        </div>
     );
 };
 
