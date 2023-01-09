3cd6900cca908b9292081afad2133ac04552b89a
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 18:36:51 +0100

    feat(#518): publishing status api

--- anni/src/pages/api/publish.ts
+++ anni/src/pages/api/publish.ts
@@ -6,15 +6,22 @@ import dbConnect from '../../database/helpers/connect';
 import AppData from '../../database/models/AppData';
 import Drug from '../../database/models/Drug';
 
+const getResolvedDrugs = async () => {
+    const drugs = await Drug!.find({ isStaged: true }).exec();
+    return await Promise.all(drugs.map((drug) => drug.resolve(pharMeLanguage)));
+};
+
 const api: NextApiHandler = async (req, res) =>
     await handleApiMethods(req, res, {
+        GET: async () => {
+            await dbConnect();
+            await getResolvedDrugs();
+            return { successStatus: 200 };
+        },
         POST: async () => {
             await dbConnect();
-            const drugs = await Drug!.find({ isStaged: true }).exec();
-            const resolved = await Promise.all(
-                drugs.map((drug) => drug.resolve(pharMeLanguage)),
-            );
-            await AppData!.publish({ drugs: resolved });
+            const drugs = await getResolvedDrugs();
+            await AppData!.publish({ drugs });
             return { successStatus: 201 };
         },
     });
