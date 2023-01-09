9e577616e1477719e82c99c401b52267f6029a46
Jannis Baum <jannis.baum@gmail.com>
2023-01-09 20:26:31 +0100

    refactor(#518): publishing status api typing

--- anni/src/pages/api/publish.ts
+++ anni/src/pages/api/publish.ts
@@ -1,10 +1,20 @@
 import { NextApiHandler } from 'next';
 
-import { handleApiMethods } from '../../common/api-helpers';
+import {
+    ApiResponse,
+    errorObject,
+    handleApiMethods,
+} from '../../common/api-helpers';
 import { pharMeLanguage } from '../../common/definitions';
 import dbConnect from '../../database/helpers/connect';
 import AppData from '../../database/models/AppData';
 import Drug from '../../database/models/Drug';
+
+interface GetReponseData {
+    errorMessage: string | null;
+}
+
+export type GetPublishStatusReponse = ApiResponse<GetReponseData>;
 
 const getResolvedDrugs = async () => {
     const drugs = await Drug!.find({ isStaged: true }).exec();
@@ -15,8 +25,14 @@ const api: NextApiHandler = async (req, res) =>
     await handleApiMethods(req, res, {
         GET: async () => {
             await dbConnect();
-            await getResolvedDrugs();
-            return { successStatus: 200 };
+            const data: GetReponseData = { errorMessage: null };
+            try {
+                await getResolvedDrugs();
+            } catch (error) {
+                data.errorMessage =
+                    errorObject(error)?.message ?? 'Unknown error';
+            }
+            return { successStatus: 200, data };
         },
         POST: async () => {
             await dbConnect();
