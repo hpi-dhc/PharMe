27f24ec7c4472afd4f1632bd02466b55a4df46a5
Jannis Baum <jannis.baum@gmail.com>
2023-01-11 18:02:11 +0100

    fix(anni): missing dbconnect

--- anni/src/pages/api/v1/data.ts
+++ anni/src/pages/api/v1/data.ts
@@ -2,11 +2,13 @@ import { NextApiHandler } from 'next';
 import { ApiError } from 'next/dist/server/api-utils';
 
 import { handleApiMethods } from '../../../common/api-helpers';
+import dbConnect from '../../../database/helpers/connect';
 import AppData from '../../../database/models/AppData';
 
 const api: NextApiHandler = async (req, res) =>
     await handleApiMethods(req, res, {
         GET: async () => {
+            await dbConnect();
             const data = await AppData!.getCurrent();
             if (!data) throw new ApiError(503, 'Data not available');
             return { successStatus: 200, data };
--- anni/src/pages/api/v1/version.ts
+++ anni/src/pages/api/v1/version.ts
@@ -2,6 +2,7 @@ import { NextApiHandler } from 'next';
 import { ApiError } from 'next/dist/server/api-utils';
 
 import { ApiResponse, handleApiMethods } from '../../../common/api-helpers';
+import dbConnect from '../../../database/helpers/connect';
 import AppData from '../../../database/models/AppData';
 
 interface GetReponseData {
@@ -13,6 +14,7 @@ export type GetCurrentVersionResponse = ApiResponse<GetReponseData>;
 const api: NextApiHandler = async (req, res) =>
     await handleApiMethods(req, res, {
         GET: async () => {
+            await dbConnect();
             const version = await AppData!.getVersion();
             if (!version) throw new ApiError(503, 'Data not available');
             return { successStatus: 200, data: { version } };
