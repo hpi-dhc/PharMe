97367e749aac00798e021ff8565d2a9bd5b8b039
Jannis Baum <jannis.baum@gmail.com>
2023-01-20 17:23:13 +0100

    refactor(#501): ignore initial loading error

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,7 +3,10 @@ import 'common/module.dart';
 Future<void> main() async {
   await initServices();
   await fetchAndSaveLookups();
-  await updateCachedDrugs();
+  try {
+    await updateCachedDrugs();
+    // ignore: empty_catches
+  } catch (error) {}
   runApp(PharMeApp());
   await cleanupServices();
 }
