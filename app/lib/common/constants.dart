5baa17b67333710db7a09e472ade28454288aa55
Jannis Baum <jannis.baum@gmail.com>
2023-02-08 19:21:23 +0100

    docs(#483): add note about simulator restrictions

--- app/lib/common/constants.dart
+++ app/lib/common/constants.dart
@@ -6,6 +6,8 @@ Uri labServerUrl([String slug = '']) =>
 Uri keycloakUrl([String slug = '']) =>
     Uri.https('keycloak-pharme.dhc-lab.hpi.de', slug);
 
+// Note that sending emails will not work on the iPhone Simulator since it does
+// not have any email application installed.
 String _mailContact = 'pgx-app-validation-study@lists.myhpi.de';
 Future<void> sendEmail({String subject = ''}) async {
   await launchUrl(Uri(
