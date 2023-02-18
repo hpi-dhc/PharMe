dcefa68fb9e59a1a2c7c861e6b9c38c29217322c
Jannis Baum <jannis.baum@gmail.com>
2023-02-18 16:05:17 +0100

    feat(#484): show phenotype from lab

--- app/lib/common/pages/drug/widgets/annotation_cards/guideline.dart
+++ app/lib/common/pages/drug/widgets/annotation_cards/guideline.dart
@@ -56,7 +56,7 @@ class GuidelineAnnotationCard extends StatelessWidget {
 
   Widget _buildHeader(BuildContext context) {
     final geneDescriptions = guideline.lookupkey.keys.map((geneSymbol) =>
-        '$geneSymbol (${UserData.instance.lookups![geneSymbol]!.phenotype})');
+        '$geneSymbol (${UserData.instance.diplotypes![geneSymbol]!.phenotype})');
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       SubHeader(context.l10n.drugs_page_your_genome),
       SizedBox(height: 12),
