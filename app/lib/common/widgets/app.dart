1f6920ebed28d896b35aced683195996d41cd42b
Jannis Baum <jannis.baum@gmail.com>
2023-04-14 17:08:42 +0200

    refactor(#591): singleton app

--- app/lib/common/widgets/app.dart
+++ app/lib/common/widgets/app.dart
@@ -4,7 +4,11 @@ import '../models/metadata.dart';
 import '../module.dart' hide MetaData;
 
 class PharMeApp extends StatelessWidget {
-  PharMeApp({Key? key}) : super(key: key);
+  factory PharMeApp() => _instance;
+
+  PharMeApp._({Key? key}) : super(key: key);
+
+  static final _instance = PharMeApp._();
 
   final _appRouter = AppRouter();
   final _isLoggedIn = MetaData.instance.isLoggedIn ?? false;
