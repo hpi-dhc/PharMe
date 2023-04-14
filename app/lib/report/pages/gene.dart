f3811c39564210f3f8d4486219203879c6a472a6
Jannis Baum <jannis.baum@gmail.com>
2023-04-14 20:32:19 +0200

    feat(#567): show affected drugs

--- app/lib/report/pages/gene.dart
+++ app/lib/report/pages/gene.dart
@@ -2,43 +2,61 @@ import '../../common/module.dart';
 import '../../common/pages/drug/widgets/sub_header.dart';
 import '../../common/pages/drug/widgets/tooltip_icon.dart';
 
-class GenePage extends StatelessWidget {
-  const GenePage(this.phenotype);
+class GenePage extends HookWidget {
+  GenePage(this.phenotype)
+      : cubit = DrugListCubit(
+          initialFilter: FilterState.forGene(phenotype.geneSymbol),
+        );
+
   final CpicPhenotype phenotype;
+  final DrugListCubit cubit;
 
   @override
   Widget build(BuildContext context) {
-    return pageScaffold(
-        title: context.l10n.gene_page_headline(phenotype.geneSymbol),
-        body: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SubHeader(
-                context.l10n.gene_page_your_variant(phenotype.geneSymbol),
-                tooltip:
-                    context.l10n.gene_page_name_tooltip(phenotype.geneSymbol),
+    return BlocProvider(
+      create: (context) => cubit,
+      child: BlocBuilder<DrugListCubit, DrugListState>(
+        builder: (context, state) => pageScaffold(
+          title: context.l10n.gene_page_headline(phenotype.geneSymbol),
+          body: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SubHeader(
+                    context.l10n.gene_page_your_variant(phenotype.geneSymbol),
+                    tooltip: context.l10n
+                        .gene_page_name_tooltip(phenotype.geneSymbol),
+                  ),
+                  SizedBox(height: 12),
+                  RoundedCard(
+                    child: Table(
+                      columnWidths: Map.from({
+                        0: IntrinsicColumnWidth(),
+                        1: IntrinsicColumnWidth(flex: 1),
+                      }),
+                      children: [
+                        _buildRow(
+                            context.l10n.gene_page_genotype, phenotype.genotype,
+                            tooltip: context.l10n.gene_page_genotype_tooltip),
+                        _buildRow(context.l10n.gene_page_phenotype,
+                            UserData.phenotypeFor(phenotype.geneSymbol)!,
+                            tooltip: context.l10n.gene_page_phenotype_tooltip),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  SubHeader(context.l10n.gene_page_affected_drugs,
+                      tooltip: context.l10n.gene_page_affected_drugs_tooltip),
+                  ...buildDrugList(context, state)
+                ],
               ),
-              SizedBox(height: 12),
-              RoundedCard(
-                child: Table(
-                    columnWidths: Map.from({
-                      0: IntrinsicColumnWidth(),
-                      1: IntrinsicColumnWidth(flex: 1),
-                    }),
-                    children: [
-                      _buildRow(
-                          context.l10n.gene_page_genotype, phenotype.genotype,
-                          tooltip: context.l10n.gene_page_genotype_tooltip),
-                      _buildRow(context.l10n.gene_page_phenotype,
-                          UserData.phenotypeFor(phenotype.geneSymbol)!,
-                          tooltip: context.l10n.gene_page_phenotype_tooltip),
-                    ]),
-              ),
-            ]),
-          ),
-        ]);
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   TableRow _buildRow(String key, String value, {String? tooltip}) =>
