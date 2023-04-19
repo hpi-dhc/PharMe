8836ad1fa9e718d43f7a228f324e054f296e621c
Jannis Baum <jannis.baum@gmail.com>
2023-04-19 16:27:33 +0200

    feat(#523): find drug annotations for bricks

--- anni/src/pages/api/annotations/index.ts
+++ anni/src/pages/api/annotations/index.ts
@@ -3,7 +3,7 @@ import { NextApiHandler } from 'next';
 import { ApiResponse, handleApiMethods } from '../../../common/api-helpers';
 import { CurationState } from '../../../database/helpers/annotations';
 import dbConnect from '../../../database/helpers/connect';
-import Drug, { IDrug_Str } from '../../../database/models/Drug';
+import Drug, { IDrug_DB, IDrug_Str } from '../../../database/models/Drug';
 
 interface ResponseData {
     drugs: Array<
@@ -14,24 +14,30 @@ interface ResponseData {
 }
 export type GetAnnotationsReponse = ApiResponse<ResponseData>;
 
+export const drugAnnotationsResponseData = async (
+    drugs: IDrug_DB[],
+): Promise<ResponseData> => {
+    const curationStates = await Promise.all(
+        drugs.map((drug) => drug.curationState()),
+    );
+    return {
+        drugs: drugs.map((drug, index) => {
+            return {
+                _id: drug._id!.toString(),
+                name: drug.name,
+                isStaged: drug.isStaged,
+                curationState: curationStates[index],
+            };
+        }),
+    };
+};
+
 const api: NextApiHandler = async (req, res) =>
     await handleApiMethods(req, res, {
         GET: async () => {
             await dbConnect();
             const drugs = await Drug!.find({}).orFail().exec();
-            const curationStates = await Promise.all(
-                drugs.map((drug) => drug.curationState()),
-            );
-            const data: ResponseData = {
-                drugs: drugs.map((drug, index) => {
-                    return {
-                        _id: drug._id!.toString(),
-                        name: drug.name,
-                        isStaged: drug.isStaged,
-                        curationState: curationStates[index],
-                    };
-                }),
-            };
+            const data = await drugAnnotationsResponseData(drugs);
             return { successStatus: 200, data };
         },
     });
--- anni/src/pages/api/bricks/[id].ts
+++ anni/src/pages/api/bricks/[id].ts
@@ -1,9 +1,30 @@
 import { NextApiHandler } from 'next';
 
 import { updateDeleteApi } from '../../../common/api-helpers';
+import dbConnect from '../../../database/helpers/connect';
+import Drug from '../../../database/models/Drug';
 import TextBrick from '../../../database/models/TextBrick';
+import { drugAnnotationsResponseData } from '../annotations/index';
 
 const api: NextApiHandler = async (req, res) =>
-    await updateDeleteApi(TextBrick!, req, res);
+    await updateDeleteApi(TextBrick!, req, res, {
+        GET: async () => {
+            await dbConnect();
+            const {
+                query: { id },
+            } = req;
+            const drugs = await Drug!
+                .find({
+                    $or: [
+                        { 'annotations.drugclass': { $in: [id] } },
+                        { 'annotations.indication': { $in: [id] } },
+                    ],
+                })
+                .exec();
+
+            const data = await drugAnnotationsResponseData(drugs);
+            return { successStatus: 200, data };
+        },
+    });
 
 export default api;
