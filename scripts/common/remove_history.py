3dac952659a19ef26b49026a806a3f114f8d19c6
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-05-05 18:24:46 +0200

    feat(#605): remove tables when removing history

--- scripts/common/remove_history.py
+++ scripts/common/remove_history.py
@@ -1,10 +1,11 @@
 def remove_history(data):
-    if 'AppData' in data:
-        data['AppData'] = []
+    tables_to_be_deleted = []
     for table_name in data.keys():
-        if table_name.endswith('_History'):
-            data[table_name] = []
+        if table_name.startswith('AppData') or table_name.endswith('_History'):
+            tables_to_be_deleted.append(table_name)
         else:
             for item in data[table_name]:
                 item['_v'] = 1
+    for table_name in tables_to_be_deleted:
+        del data[table_name]
     return data
