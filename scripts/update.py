f64040876aeac519ce40bd397b146679dd30e9a5
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-05-08 16:01:35 +0200

    feat(#608): update RxNorm

--- scripts/update.py
+++ scripts/update.py
@@ -20,21 +20,43 @@ def update_data():
     data = remove_history(get_data())
     updated_external_data = get_data(argv_index = 2)
     log_content = ['# Update Log\n\n']
-    present_drugs = list(map(
-        lambda drug: drug['name'], data[DRUG_COLLECTION_NAME]))
+    present_drugs = {}
+    for drug in data[DRUG_COLLECTION_NAME]:
+        present_drugs[drug['name']] = drug
     for drug in updated_external_data[DRUG_COLLECTION_NAME]:
         drug_name = drug['name']
+        drug_guidelines = get_guidelines_by_ids(
+            updated_external_data, drug['guidelines'])
         log_string = f'* {drug_name}: '
         if not drug_name in present_drugs:
             data[DRUG_COLLECTION_NAME].append(drug)
-            drug_guidelines = get_guidelines_by_ids(
-                updated_external_data, drug['guidelines'])
             data[GUIDELINE_COLLECTION_NAME] += drug_guidelines
             log_string += 'not present; added drug with ' \
                 f'{len(drug_guidelines)} empty annotation(s)'
+            log_content.append(f'{log_string}\n')
         else:
-            log_string += 'present; NOT IMPLEMENTED: check for updates'
-        log_content.append(f'{log_string}\n')
+            update_log = []
+            present_drug = present_drugs[drug_name]
+
+            # Test if RxNorm changed
+            present_rxNorm = present_drug['rxNorm']
+            rxNorm = drug['rxNorm']
+            if present_rxNorm != rxNorm:
+                update_log.append(f'* Change RxNorm from {present_rxNorm} ' \
+                                  f'to {rxNorm}\n')
+                present_drug['rxNorm'] = drug['rxNorm']
+
+            # TODO: Check guidelines for updates; key should be phenotype
+            present_guidelines = get_guidelines_by_ids(
+                data, present_drug['guidelines'])
+
+            # Write update log
+            if len(update_log) == 0:
+                log_string += 'no updates'
+            else:
+                log_string += 'updated:'
+            log_content.append(f'{log_string}\n')
+            log_content += update_log
     write_data(data, postfix=SCRIPT_POSTFIXES['update'])
     write_log(log_content)
 
