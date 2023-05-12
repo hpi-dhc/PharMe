7ae74f90ae6b650a1d801b52af575cd31ad9cb23
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-05-12 13:20:06 +0200

    fix(anni/backupper): fix backupper if newly initialized

--- anni/backupper/run.sh
+++ anni/backupper/run.sh
@@ -4,16 +4,16 @@ ROOT_DIR=$(dirname $(realpath $0))
 
 . $ROOT_DIR/.env
 
-git config user.email $GITHUB_USER_EMAIL
-git config user.name $GITHUB_USER_NAME
-
 test -d $BACKUP_DIR \
     || git clone https://oauth2:$GITHUB_OAUTH@$GITHUB_URL $BACKUP_DIR
 
 cd $BACKUP_DIR
 
-curl -s $ANNI_URL/api/backup | jq -r '.data.base64' | base64 -D > backup.zip
-unzip backup.zip
+git config user.email $GITHUB_USER_EMAIL
+git config user.name $GITHUB_USER_NAME
+
+curl -s $ANNI_URL/api/backup | jq -r '.data.base64' | base64 -d > backup.zip
+unzip -o backup.zip
 rm backup.zip
 
 git add --all
