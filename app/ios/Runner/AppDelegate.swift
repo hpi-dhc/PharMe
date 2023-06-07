cc05744edc560daf025f88082576377ca56b7d8c
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-06-07 15:10:58 +0200

    Revert "feat: add requirements for flutter_sharing_intent package"
    This reverts commit 1e3d8ee0ea281c09cd338f48e93b0c5f429d554a.

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import flutter_sharing_intent
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,15 +10,4 @@ import flutter_sharing_intent
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
-
-override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-  let sharingIntent = SwiftFlutterSharingIntentPlugin.instance
-  /// if the url is made from SwiftFlutterSharingIntentPlugin then handle it with plugin [SwiftFlutterSharingIntentPlugin]
-  if sharingIntent.hasSameSchemePrefix(url: url) {
-      return sharingIntent.application(app, open: url, options: options)
-  }
-
-  // Proceed url handling for other Flutter libraries like uni_links
-  return super.application(app, open: url, options:options)
 }
