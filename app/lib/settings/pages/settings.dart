2de33a47d8ef10e42227e2de2221d86fb9b6e691
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-08-24 16:14:48 +0200

    feat(#650): add checkbox when deleting data

--- app/lib/settings/pages/settings.dart
+++ app/lib/settings/pages/settings.dart
@@ -19,7 +19,7 @@ class SettingsPage extends StatelessWidget {
         trailing: Icon(Icons.chevron_right_rounded),
         onTap: () => showDialog(
           context: context,
-          builder: (_) => _deleteDataDialog(context),
+          builder: (_) => DeleteDataDialog(),
         ),
       ),
       Divider(),
@@ -57,25 +57,47 @@ class SettingsPage extends StatelessWidget {
           onTap: sendEmail)
     ]);
   }
+}
 
-  Widget _deleteDataDialog(BuildContext context) {
+class DeleteDataDialog extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final agreedToDeletion = useState(false);
+
     return AlertDialog(
       title: Text(context.l10n.settings_page_delete_data),
-      content: Text(context.l10n.settings_page_delete_data_text),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(context.l10n.settings_page_delete_data_text),
+          SizedBox(height: PharMeTheme.mediumSpace),
+          CheckboxListTile(
+            value: agreedToDeletion.value,
+            onChanged: (value) => agreedToDeletion.value = value
+              ?? agreedToDeletion.value,
+            title: Text(context.l10n.settings_page_delete_data_confirmation),
+            controlAffinity: ListTileControlAffinity.leading,
+            contentPadding: EdgeInsets.zero,
+          ),
+        ]),
       actions: [
         TextButton(
           onPressed: context.router.root.pop,
           child: Text(context.l10n.action_cancel),
         ),
         TextButton(
-          onPressed: () async {
-            await deleteAllAppData();
-            // ignore: use_build_context_synchronously
-            await context.router.replaceAll([LoginRouter()]);
-          },
+          onPressed: agreedToDeletion.value
+            ? () async {
+              await deleteAllAppData();
+              // ignore: use_build_context_synchronously
+              await context.router.replaceAll([LoginRouter()]);
+            }
+            : null,
           child: Text(
             context.l10n.action_continue,
-            style: TextStyle(color: PharMeTheme.errorColor),
+            style: agreedToDeletion.value
+              ? TextStyle(color: PharMeTheme.secondaryColor)
+              : TextStyle(color: PharMeTheme.onSurfaceColor),
           ),
         ),
       ],
