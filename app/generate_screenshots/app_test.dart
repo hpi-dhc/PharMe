436d215b15639da1884309644ed6665158f3a0ae
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-12-20 14:42:38 +0100

    chore(docs): add idea how to enable screenshot generation

--- app/generate_screenshots/app_test.dart
+++ app/generate_screenshots/app_test.dart
@@ -52,6 +52,8 @@ void main() {
       await takeScreenshot(tester, binding, 'login');
 
       // login-redirect (not working; only taking screenshot of loading screen)
+      // could try to use cubit function to directly sign in which will only
+      // open the webview and close it again
       // await tester.tap(find.byType(FullWidthButton).first);
       // await Future.delayed(Duration(seconds: 3)); // wait for dialog
       // await takeScreenshot(tester, binding, 'login-redirect');
