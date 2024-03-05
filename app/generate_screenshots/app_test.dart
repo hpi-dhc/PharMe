9dbc0d52b389660c7f0df4c2c49c2ef444da9362
Tamara Slosarek <tamara.slosarek@hpi.de>
2024-03-05 19:21:12 +0100

    chore(#687): remove unsused import

--- app/generate_screenshots/app_test.dart
+++ app/generate_screenshots/app_test.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 
 import 'package:app/app.dart';
 import 'package:app/common/module.dart';
-import 'package:dartx/dartx_io.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:provider/provider.dart';
