1c76781386f0796c679e05fb5fedae0fdd240b67
tamara-slosarek <tamara.slosarek@mssm.edu>
2024-03-18 22:42:40 +0100

    feat(#687): cut at keyframes

--- app/generate_screendocs/generate_screencast.sh
+++ app/generate_screendocs/generate_screencast.sh
@@ -2,136 +2,182 @@
 
 output_directory="../docs/screencasts/"
 cuts_log_path="${output_directory}cuts.log"
-test_log_path="${output_directory}test.log"
+flutter_log_path="${output_directory}flutter.log"
 full_video_path="${output_directory}full.mov"
 
+end_offset=3
+
+timestamp_prefix="TIMESTAMP: "
+
 ffmpeg_log_level="warning"
-
-# Time margin to be added, if needed
-end_offset=0
-start_offset=0
-
-read -p "Enter username: " username
-read -p "Enter password: " password
+redo_recording=true # should be true, set false to only test cutting
 
 log_timestamp() {
     echo "$(date +%s000) $1" >> "$cuts_log_path"
 }
 
-# Doing the recording inspired by
-# https://betterprogramming.pub/how-to-record-flutter-integration-tests-with-github-actions-1ca670eff94a
+if $redo_recording; then
 
-# Start recording
+    read -p "Enter username: " username
+    read -p "Enter password: " password
 
+    # Doing the recording was inspired by
+    # https://betterprogramming.pub/how-to-record-flutter-integration-tests-with-github-actions-1ca670eff94a
 
-rm "$cuts_log_path"
-xcrun simctl io booted recordVideo "$full_video_path" -f &
-log_timestamp "recording_start"
-sleep 5
-export RECORDING_PID=${!}
-echo "Recording process up with pid: ${RECORDING_PID}"
-echo "Running app"
-flutter drive \
-    --driver=generate_screendocs/test_driver.dart \
-    --target=generate_screendocs/screencast_sequence.dart \
-    --dart-define=TEST_USER="$username" \
-    --dart-define=TEST_PASSWORD="$password" | tee "$test_log_path"
+    # Start recording
 
-# Write cut timestamps from test.log to cut.log
+    rm -f "$cuts_log_path"
+    rm -f "$flutter_log_path"
+    xcrun simctl io booted recordVideo "$full_video_path" -f &
+    log_timestamp "recording_start"
+    sleep 5
+    export RECORDING_PID=${!}
+    echo "Recording process up with pid: ${RECORDING_PID}"
+    echo "Running app"
+    flutter drive \
+        --driver=generate_screendocs/test_driver.dart \
+        --target=generate_screendocs/screencast_sequence.dart \
+        --dart-define=TIMESTAMP_PREFIX="$timestamp_prefix" \
+        --dart-define=START_OFFSET="$end_offset" \
+        --dart-define=TEST_USER="$username" \
+        --dart-define=TEST_PASSWORD="$password" | tee "$flutter_log_path"
 
-echo "Finishing recording"
-timestamp_prefix="flutter: TIMESTAMP: "
-extracted_timestamps=$(\
-awk -v s="$timestamp_prefix" 'index($0, s) == 1' "$test_log_path" | \
-sed -e "s/^$timestamp_prefix//"\
-)
-echo "$extracted_timestamps"  >> "$cuts_log_path"
-rm "$test_log_path"
+    # Write cut timestamps from test.log to cut.log
 
-# End recording
+    echo "Finishing recording"
 
-log_timestamp "recording_end"
-sleep 5
-kill -SIGINT $RECORDING_PID
-sleep 10
-echo ""
+    logged_timestamp_prefix="flutter: $timestamp_prefix"
+    extracted_timestamps=$(\
+        awk -v s="$logged_timestamp_prefix" 'index($0, s) == 1' "$flutter_log_path" | \
+        sed -e "s/^$logged_timestamp_prefix//"\
+    )
+    echo "$extracted_timestamps"  >> "$cuts_log_path"
+
+    # End recording
+
+    log_timestamp "recording_end"
+    sleep 5
+    kill -SIGINT $RECORDING_PID
+    sleep 10
+    echo ""
+
+fi
 
 # Cut smaller screencasts
 
-echo "Cutting smaller screencasts"
+echo "Cutting smaller screencasts at keyframes closest to logged times"
+
 timestamps=()
 descriptions=()
 while read cut_info; do
-  cut_info_array=($cut_info)
-  timestamp=${cut_info_array[0]}
-  description=${cut_info_array[1]}
-  timestamps+=("$timestamp")
-  descriptions+=("$description")
+    if [ -z "$cut_info" ]; then
+        continue
+    fi
+    cut_info_array=($cut_info)
+    timestamp=${cut_info_array[0]}
+    description=${cut_info_array[1]}
+    timestamps+=("$timestamp")
+    descriptions+=("$description")
 done < "$cuts_log_path"
 
-get_seconds_since_start() {
-    echo $((($1-${timestamps[0]})/1000))
+get_frame_starts() {
+    local video_path=$1
+    echo $(
+        ffprobe -loglevel "$ffmpeg_log_level" \
+            -select_streams v \
+            -show_entries frame=pts_time \
+            -of csv=print_section=0 \
+            "$video_path" \
+            | awk -F',' '{print $1}'
+    )
 }
 
-# Format seconds as hh:mm:ss, see https://stackoverflow.com/a/13425821
-format_seconds_as_time() {
-    ((
-        sec=$1%60,
-        $1/=60,
-        min=$1%60,
-        hrs=$1/60
-    ))
-    echo $(printf "%02d:%02d:%02d" $hrs $min $sec)
+get_frame_durations() {
+    local video_path=$1
+    echo $(
+        ffprobe -loglevel "$ffmpeg_log_level" \
+            -select_streams v \
+            -show_entries frame=pkt_duration_time \
+            -of csv=print_section=0 \
+            "$video_path" \
+            | awk -F',' '{print $1}'
+    )
+}
+
+frame_starts=($(get_frame_starts $full_video_path))
+frame_durations=($(get_frame_durations $full_video_path))
+
+get_difference() {
+    local first_float=$1
+    local second_float=$2
+    local difference=$(echo "$first_float - $second_float" | bc)
+    echo ${difference#-}
+}
+
+get_closest_frame_start() {
+    local target=$1
+    local best_fit_index=0
+    local smallest_difference=$(get_difference $target ${frame_starts[0]})
+    for ((j=1; j<${#frame_starts[@]}; j++)); do
+        current_difference=$(get_difference $target ${frame_starts[j]})
+        if (( $(echo "$current_difference < $smallest_difference" | bc) )); then
+            smallest_difference=$current_difference
+            best_fit_index=$j
+        fi
+    done
+    echo "${frame_starts[best_fit_index]}"
+}
+
+get_frame_end() {
+    local index=$1
+    local frame_start=${frame_starts[index]}
+    local frame_duration=${frame_durations[index]}
+    echo $(echo "$frame_start + $frame_duration" | bc)
+}
+
+get_closest_frame_end() {
+    local target=$1
+    local best_fit_index=0
+    local smallest_difference=$(get_difference $target $(get_frame_end 0))
+    for ((j=1; j<${#frame_starts[@]}; j++)); do
+        current_difference=$(get_difference $target $(get_frame_end $j))
+        if (( $(echo "$current_difference < $smallest_difference" | bc) )); then
+            smallest_difference=$current_difference
+            best_fit_index=$j
+        fi
+    done
+    echo "$(get_frame_end $best_fit_index)"
+}
+
+get_seconds_since_start() {
+    echo $(printf %.3f "$(($1-${timestamps[0]}))e-3")
 }
 
 cut_video() {
-    description=$1
-    start_seconds=$2
-    end_seconds=$3
-    ((start_seconds-=$start_offset))
-    ((end_seconds+=$end_offset))
-    if [ $start_seconds -eq $end_seconds ]; then
-        ((end_seconds+=1))
-    fi
-    total_seconds=$(($end_seconds-$start_seconds))
-    start_time=$(format_seconds_as_time start_seconds)
-    end_time=$(format_seconds_as_time end_seconds)
-    total_time=$(format_seconds_as_time total_seconds)
-    echo "Cutting $description ($total_time, $start_time – $end_time)"
-    video_path="${output_directory}${description}.mp4"
+    local description=$1
+    local start_seconds=$(get_closest_frame_start $2)
+    local exact_end_with_offset=$(echo "$3 + $end_offset" | bc)
+    local end_seconds=$(get_closest_frame_end $exact_end_with_offset)
+
+    echo "Cutting $description $start_seconds – $end_seconds (originally $2 – $3, with offset $exact_end_with_offset)"
+
+    local video_path="${output_directory}${description}.mp4"
+
     ffmpeg -y -loglevel "$ffmpeg_log_level" -an \
         -i "$full_video_path" \
-        -pix_fmt yuv420p -vcodec h264 -crf 21 \
-        -ss "$start_time" \
-        -to "$end_time" \
-        -avoid_negative_ts make_zero -async 1 \
+        -ss "$start_seconds" \
+        -to "$end_seconds" \
         "$video_path"
-
-    # Starting at first keyframe does not work as intended,
-    # but leaving this code here in case somebody will try to cut out the
-    # black frames in the beginning
-    # first_keyframe=$(
-    #     ffprobe -loglevel "$ffmpeg_log_level" \
-    #         -select_streams v \
-    #         -show_entries packet=pts_time \
-    #         -of csv=print_section=0 \
-    #         "$video_path" \
-    #         | awk -F',' '{print $1}' \
-    #         | head -1
-    # )
-    # echo $first_keyframe
 }
 
 cut_video "full_clean" \
     $(get_seconds_since_start ${timestamps[1]}) \
     $(get_seconds_since_start ${timestamps[${#timestamps[@]}-2]})
 
-for ((i=2; i<${#timestamps[@]}-1; i++))
-do
+# Skip first and last entries (recording_start and recording_end)
+for ((i=2; i<${#timestamps[@]}-1; i++)); do
     description=${descriptions[i]}
     start_seconds=$(get_seconds_since_start ${timestamps[i-1]})
-    ((start_seconds-=$start_offset))
     end_seconds=$(get_seconds_since_start ${timestamps[i]})
-    ((end_seconds+=$end_offset))
     cut_video $description $start_seconds $end_seconds
 done
