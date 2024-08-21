b3ffff4dacb65eda5664663626808f58389d939a
tamara-slosarek <tamara.slosarek@mssm.edu>
2024-08-21 12:43:06 +0200

    feat(scripts): also print CPIC alleles

--- scripts/cpic_lookup_genes.py
+++ scripts/cpic_lookup_genes.py
@@ -1,11 +1,25 @@
 import json
 import requests
 
-cpicLookupUrl = "https://api.cpicpgx.org/v1/diplotype?select=genesymbol"
-response = requests.get(cpicLookupUrl)
+cpicLookupUrl = "https://api.cpicpgx.org/v1/diplotype"
+
+print('CPIC genes:')
+response = requests.get(f'{cpicLookupUrl}?select=genesymbol')
 cpicLookups = response.json()
 genes = set()
 for lookup in cpicLookups:
     genes.add(lookup['genesymbol'])
 for gene in genes:
     print(gene)
+
+print('')
+print('Covered alleles per gene:')
+for gene in genes:
+    alleles = set()
+    response = requests.get(f'{cpicLookupUrl}?select=diplotype&genesymbol=eq.{gene}')
+    results = response.json()
+    for result in results:
+        diplotype = result['diplotype']
+        for allele in diplotype.split('/'):
+            alleles.add(allele.strip())
+    print(f'{gene}: {";".join(alleles)}')
