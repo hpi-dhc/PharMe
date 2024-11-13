16eed489273408123eea6290669f87fc65e336a0
tamslo <tamaraslosarek@gmail.com>
2024-11-13 19:07:38 +0100

    chore(#661): move TODOs to issue

--- app/lib/login/models/app_share_flow_lab.dart
+++ app/lib/login/models/app_share_flow_lab.dart
@@ -13,10 +13,6 @@ class AppShareFlowLab extends Lab {
 
   @override
   Future<void> authenticate() async {
-    // TODO: introduce static properties for loading message and whether loading
-    // can be canceled (throws LabAuthenticationCanceled)
-    // TODO: get query data from DWA via deeplink (Android-only suffient)
-    // TODO: show error for iOS until implemented
     // THIS IS FOR TESTING, SHOULD GET FROM DWA
     publishUrl = Uri.parse(
       'https://hpi-datastore.duckdns.org/userdata?id=1e006a69-b693-43d2-a318-22904e305b5c',
